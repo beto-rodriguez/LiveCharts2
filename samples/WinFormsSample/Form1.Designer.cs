318fa5e1de469ca0ba23a96c3dfb5ca416e9dd18
BetoRO <rodriguezo.alberto@gmail.com>
2021-02-13 17:39:29 -0600

    winforms net 5

---
+++ samples/WinFormsSample/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace WinFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart1.AnimationsSpeed = System.TimeSpan.Parse("00:00:00.5000000");
+            this.cartesianChart1.DrawMargin = null;
+            this.cartesianChart1.LegendOrientation = LiveChartsCore.Context.LegendOrientation.Auto;
+            this.cartesianChart1.LegendPosition = LiveChartsCore.Context.LegendPosition.None;
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(761, 421);
+            this.cartesianChart1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 445);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart cartesianChart1;
+    }
+}
+
---
+++ samples/WinFormsSample/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ViewModelsSamples;
+
+namespace WinFormsSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var vm = new MainVM();
+
+            cartesianChart1.Series = vm.Series;
+            cartesianChart1.XAxes = vm.XAxes;
+            cartesianChart1.YAxes = vm.YAxes;
+        }
+    }
+}
