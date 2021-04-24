0d79f01764aab21cddf06b54c0aea4899b9f4fd5
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-04-23 19:16:32 -0500

    added xamarin missing samples

---
+++ samples/XamarinSample/XamarinSample/XamarinSample/Axes/LabelsRotation/View.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSample.Axes.LabelsRotation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View : ContentPage
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/XamarinSample/XamarinSample/XamarinSample/Axes/Multiple/View.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSample.Axes.Multiple
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View : ContentPage
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
