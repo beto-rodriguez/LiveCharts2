41260b6577223cccc5010e18910d41f2059c9e9c
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-05-29 09:11:27 -0500

    docs

--- samples/ViewModelsSamples/Bars/WithBackground/ViewModel.cs
+++ samples/ViewModelsSamples/Bars/WithBackground/ViewModel.cs
@@ -3,6 +3,7 @@ using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ViewModelsSamples.Bars.WithBackground
 {
@@ -12,18 +13,23 @@ namespace ViewModelsSamples.Bars.WithBackground
         {
             new ColumnSeries<double>
             {
-                Values = new List<double> { 10, 10, 10, 10, 10, 10, 10 },
+                Values = new ObservableCollection<double> { 10, 10, 10, 10, 10, 10, 10 },
                 Stroke = null,
                 Fill = new SolidColorPaintTask(new SKColor(30, 30, 30, 30)),
                 IgnoresBarPosition = true
             },
             new ColumnSeries<double>
             {
-                Values = new List<double> { 3, 10, 5, 3, 7, 3, 8 },
+                Values = new ObservableCollection<double> { 3, 10, 5, 3, 7, 3, 8 },
                 Stroke = null,
                 Fill = new SolidColorPaintTask(SKColors.CornflowerBlue),
                 IgnoresBarPosition = true
             }
+        };
+
+        public List<Axis> YAxes { get; set; } = new List<Axis>
+        {
+            new Axis { MinLimit = 0, MaxLimit = 10 }
         };
     }
 }
