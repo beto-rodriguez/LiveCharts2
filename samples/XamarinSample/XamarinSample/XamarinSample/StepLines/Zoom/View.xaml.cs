74224abb146de5e36dbef1ab53e9c6eb0d382b97
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-06-16 18:58:29 -0500

    added missing sep line samples

---
+++ samples/XamarinSample/XamarinSample/XamarinSample/StepLines/Zoom/View.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSample.StepLines.Zoom
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View : ContentPage
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
