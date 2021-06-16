c3ee23fcced0c54d8961aa406e3c24952e1664f3
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-06-16 18:42:59 -0500

    added missing financial samples

---
+++ samples/WPFSample/Financial/BasicCandlesticks/View.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPFSample.Financial.BasicCandlesticks
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : UserControl
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
