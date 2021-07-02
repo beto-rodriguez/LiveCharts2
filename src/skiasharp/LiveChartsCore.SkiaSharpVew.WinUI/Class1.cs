0b9e638c824b0b3a30cfc3e2b8add19b1edd8442
Beto Rodriguez <rodriguezo.alberto@gmail.com>
2021-07-01 19:40:31 -0500

    added projects and references

---
+++ src/skiasharp/LiveChartsCore.SkiaSharpVew.WinUI/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LiveChartsCore.SkiaSharpVew.WinUI
+{
+    public class Class1
+    {
+    }
+}
