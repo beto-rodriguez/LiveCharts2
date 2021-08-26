e9bdba413bea926a438280cea7d8ed45f188a060
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-08-26 18:42:04 -0500

    motion canvas

--- src/skiasharp/LiveChartsCore.SkiaSharpView.Maui/CartesianChart.xaml.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharpView.Maui/CartesianChart.xaml.cs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Xaml;
 using SkiaSharp;
