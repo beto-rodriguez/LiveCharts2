510da161dbf9291234a2f5daa88a89f2a865bf90
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-08-29 16:32:02 -0500

    ..

--- samples/MauiSample/MauiSample/MauiSample/MauiSample/AppShell.xaml.cs
+++ samples/MauiSample/MauiSample/MauiSample/MauiSample/AppShell.xaml.cs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System.Diagnostics;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Xaml;
 
