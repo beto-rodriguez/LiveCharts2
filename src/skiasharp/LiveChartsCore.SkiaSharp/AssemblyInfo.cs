d5f95919bca6ddae6816871c7c678fa68624b999
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2021-12-04 18:51:54 -0600

    sign release build

--- src/skiasharp/LiveChartsCore.SkiaSharp/AssemblyInfo.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharp/AssemblyInfo.cs
@@ -20,6 +20,19 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
+#if DEBUG
+
 [assembly: InternalsVisibleTo("LiveChartsBackersPackage")]
+
+#endif
+
+#if RELEASE
+
+[assembly: AssemblyKeyFile("./../../../../../../nupkgs/keypair.snk")]
+[assembly: InternalsVisibleTo("LiveChartsBackersPackage, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5845b22b111de8f9504daf136f975d790af5361b9612a3dea48fc3480087efa128adbfe0267ee4f894d8985e8b9c55f6734896e192fa647e142e1aadf281131d2329b345e456bbb6ff5b3ef4bf8da574022d0e9618a68c9241a61350bb505b6ae6a49f6c3d45be7aa76f5646574d40870c22d648d57765e040cac6e8b21c3d3")]
+
+#endif
+
