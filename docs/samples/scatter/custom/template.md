bb91de0f812e5b03111c89d4626b29eca98c0047
beto rodriguez <rodriguezo.alberto@gmail.com>
2021-12-17 17:50:54 -0600

    bubbles and custom scatter samples

---
+++ docs/samples/scatter/custom/template.md
@@ -0,0 +1,1 @@
+{{ render this "~/shared/genericSampleSimple.md" }}
--- samples/ViewModelsSamples/Scatter/Bubbles/ViewModel.cs
+++ samples/ViewModelsSamples/Scatter/Bubbles/ViewModel.cs
@@ -16,26 +16,40 @@ public class ViewModel
         var values1 = new ObservableCollection<WeightedPoint>();
         var values2 = new ObservableCollection<WeightedPoint>();
 
-        for (var i = 0; i < 20; i++)
+        for (var i = 0; i < 8; i++)
         {
-            values1.Add(new WeightedPoint(r.Next(0, 20), r.Next(0, 20), r.Next(0, 20)));
-            values2.Add(new WeightedPoint(r.Next(0, 20), r.Next(0, 20), r.Next(0, 20)));
+            // the WeightedPoint has 3 properties, X, Y and Weight
+            // we use the X and Y coordinates to position the point in the chart
+            // the weight will be used by the library to define the size of the points
+
+            // where the minimum weight will be the smallest point, and the max weight the biggest
+
+            // for any weight between these limits the library
+            // will interpolate lineally to determine the size of each point
+
+            var x = r.Next(0, 20);
+            var y = r.Next(0, 20);
+            var w = r.Next(0, 100);
+
+            values1.Add(new WeightedPoint(x, y, w));
+
+            // we do the same for our seconds values collection.
+            values2.Add(new WeightedPoint(r.Next(0, 20), r.Next(0, 20), r.Next(0, 100)));
         }
 
         Series = new ObservableCollection<ISeries>
             {
-                new ScatterSeries<WeightedPoint, RoundedRectangleGeometry>
+                new ScatterSeries<WeightedPoint>
                 {
                     Values = values1,
-                    GeometrySize = 40,
-                    MinGeometrySize = 15
+                    GeometrySize = 50,
+                    MinGeometrySize = 5
                 },
-
-                new ScatterSeries<WeightedPoint, CircleGeometry>
+                new ScatterSeries<WeightedPoint, RoundedRectangleGeometry>
                 {
                     Values = values2,
-                    GeometrySize = 40,
-                    MinGeometrySize = 15
+                    GeometrySize = 50,
+                    MinGeometrySize = 5
                 }
             };
     }
