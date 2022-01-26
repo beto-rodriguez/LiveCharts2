e35f91c19c7848627217e773a822714481b17f6d
Tore Fuglestad <tore@fuglestad.no>
2022-01-26 18:40:28 +0100

    dont need explicit scrollable, listbox will make it automatically

--- samples/EtoFormsSample/Form1.cs
+++ samples/EtoFormsSample/Form1.cs
@@ -6,8 +6,8 @@ namespace EtoFormsSample;
 
 public class Form1 : Form
 {
-    private readonly Scrollable _scroll = new Scrollable();
-    private readonly DynamicLayout _layout = new DynamicLayout();
+    private readonly ListBox _listbox = new();
+    private readonly DynamicLayout _layout = new();
 
     public Form1()
     {
@@ -15,19 +15,15 @@ public class Form1 : Form
 
         Size = new Size(800, 600);
 
-        var listbox = new ListBox();
         foreach (var item in ViewModelsSamples.Index.Samples)
         {
-            listbox.Items.Add(item);
+            _listbox.Items.Add(item);
         }
 
-        _scroll = new Scrollable() { Content = listbox };
-        listbox.MouseWheel += (o, e) => { _scroll.ScrollPosition -= new Point(0, (int)e.Delta.Height * 48); };
-
-        _layout.AddRow(_scroll, new Panel());
+        _ = _layout.AddRow(_listbox, new Panel());
         Content = _layout;
 
-        listbox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+        _listbox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
     }
 
     private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -36,7 +32,7 @@ public class Form1 : Form
         var chart = (Control)Activator.CreateInstance(null, $"EtoFormsSample.{selected.Replace('/', '.')}.View").Unwrap();
 
         var layout = new DynamicLayout();
-        layout.AddRow(_scroll, chart);
+        _ = layout.AddRow(_listbox, chart);
         Content = layout;
     }
 }
