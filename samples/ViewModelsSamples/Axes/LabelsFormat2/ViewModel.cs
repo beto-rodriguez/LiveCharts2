a026418bf5d4ad3c9117512ea8e5b76c56c50625
Ramin Alirezaee <ramin.alirezaee@hotmail.com>
2022-03-12 18:52:27 +0330

    Fix Persian and Arabic text(label) rendering

--- samples/ViewModelsSamples/Axes/LabelsFormat2/ViewModel.cs
+++ samples/ViewModelsSamples/Axes/LabelsFormat2/ViewModel.cs
@@ -22,15 +22,16 @@ public class ViewModel
             {
                 Name = "Salesman/woman",
                 NamePaint = new SolidColorPaint { Color = SKColors.Red },
-                Labels = new string[] { "王", "赵", "张" },
+                //Labels = new string[] { "王", "赵", "张" },
+                Labels = new string[] { "سرجیو", "لاندو", "لوئیس" },
                 LabelsPaint = new SolidColorPaint
                 {
                     Color = SKColors.Black,
                     // you need to enable the Chinease characters for SkiaSharp
                     // Livecharts provides the MatchChar() function that relays on the
                     // SKFontManager.Default.MatchCharacter() SkiaSharp function.
-                    FontFamily = LiveChartsSkiaSharp.MatchChar('汉') // 汉语 // mark
-                    //FontFamily = LiveChartsSkiaSharp.MatchChar('أ'), // Arab
+                    //FontFamily = LiveChartsSkiaSharp.MatchChar('汉') // 汉语 // mark
+                    FontFamily = LiveChartsSkiaSharp.MatchChar('أ'), // Arab
                     //FontFamily = LiveChartsSkiaSharp.MatchChar('あ'), // Japanease
                     //FontFamily = LiveChartsSkiaSharp.MatchChar('Ж'), // Russian
                 }
