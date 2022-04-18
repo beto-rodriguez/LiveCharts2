09b41d287efd724065521356a0da16acd20f37ca
beto rodriguez <rodriguezo.alberto@gmail.com>
2022-04-17 22:54:10 -0500

    update maui to rc1

--- samples/MauiSample/AppShell.xaml.cs
+++ samples/MauiSample/AppShell.xaml.cs
@@ -1,34 +1,5 @@
-﻿// The MIT License(MIT)
-//
-// Copyright(c) 2021 Alberto Rodriguez Orozco & LiveCharts Contributors
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+﻿namespace MauiSample;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
-
-namespace MauiSample;
-
-[XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class AppShell : Shell
 {
     private bool _isLoaded = false;
@@ -95,13 +66,4 @@ public partial class AppShell : Shell
         var c = next.Items[0].Items[0];
         c.Content = i;
     }
-
-    // UnComment the below method to handle Shell Menu item click event
-    // And ensure appropriate page definitions are available for it work as expected
-    /*
-    private async void OnMenuItemClicked(object sender, EventArgs e)
-    {
-        await Current.GoToAsync("//login");
-    }
-    */
 }
