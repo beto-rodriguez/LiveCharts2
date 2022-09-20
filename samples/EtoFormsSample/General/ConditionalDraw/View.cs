fed4791640f0865593f00e96d8d2104f024f5d49
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2022-09-19 20:51:28 -0500

    visual mappers sample in all plats

---
+++ samples/EtoFormsSample/General/ConditionalDraw/View.cs
@@ -0,0 +1,26 @@
+﻿using Eto.Forms;
+using LiveChartsCore.SkiaSharpView.Eto;
+using ViewModelsSamples.General.ConditionalDraw;
+
+namespace EtoFormsSample.General.ConditionalDraw;
+
+public class View : Panel
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="View"/> class.
+    /// </summary>
+    public View()
+    {
+        var viewModel = new ViewModel();
+
+        var cartesianChart = new CartesianChart
+        {
+            Series = viewModel.Series,
+            Sections = viewModel.Sections,
+            YAxes = viewModel.Y,
+            LegendPosition = LiveChartsCore.Measure.LegendPosition.Right,
+        };
+
+        Content = cartesianChart;
+    }
+}
---
+++ samples/WinFormsSample/General/ConditionalDraw/View.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using LiveChartsCore.SkiaSharpView.WinForms;
+using ViewModelsSamples.General.ConditionalDraw;
+
+namespace WinFormsSample.General.ConditionalDraw;
+
+public partial class View : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="View"/> class.
+    /// </summary>
+    public View()
+    {
+        InitializeComponent();
+        Size = new System.Drawing.Size(50, 50);
+
+        var viewModel = new ViewModel();
+
+        var cartesianChart = new CartesianChart
+        {
+            Series = viewModel.Series,
+            Sections = viewModel.Sections,
+            YAxes = viewModel.Y,
+            LegendPosition = LiveChartsCore.Measure.LegendPosition.Right,
+
+            // out of livecharts properties...
+            Location = new System.Drawing.Point(0, 0),
+            Size = new System.Drawing.Size(50, 50),
+            Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom
+        };
+
+        Controls.Add(cartesianChart);
+    }
+}
