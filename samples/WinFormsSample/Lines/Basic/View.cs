b24dc93d1db4f5703ee7d01661330c7c9ab61662
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2022-10-18 10:36:47 -0500

    cleaner winforms toopltip, fixes #690

--- samples/WinFormsSample/Lines/Basic/View.cs
+++ samples/WinFormsSample/Lines/Basic/View.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
+using System.Windows.Forms;
 using LiveChartsCore.SkiaSharpView.WinForms;
 using ViewModelsSamples.Lines.Basic;
 
@@ -18,14 +19,25 @@ public partial class View : UserControl
         cartesianChart = new CartesianChart
         {
             Series = viewModel.Series,
-            Title = viewModel.Title,
+            //Title = viewModel.Title,
 
             // out of livecharts properties...
-            Location = new System.Drawing.Point(0, 0),
+            Location = new System.Drawing.Point(0, 20),
             Size = new System.Drawing.Size(50, 50),
             Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom
         };
 
+        cartesianChart.SendToBack();
         Controls.Add(cartesianChart);
+
+        var a = new Button() { Text = "hello world!" };
+        a.BringToFront();
+        Controls.Add(a);
+        a.Click += A_Click;
+    }
+
+    private void A_Click(object sender, System.EventArgs e)
+    {
+        cartesianChart.Series.First().IsVisible = !cartesianChart.Series.First().IsVisible;
     }
 }
