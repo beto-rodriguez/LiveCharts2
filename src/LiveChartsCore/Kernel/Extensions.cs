c2acd244f93eb8db65dccd685286923e82fdf389
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2022-10-27 21:19:13 -0500

    merge extensions

--- src/LiveChartsCore/Kernel/Extensions.cs
+++ src/LiveChartsCore/Kernel/Extensions.cs
@@ -378,6 +378,125 @@ public static class Extensions
         return dictionary.TryGetValue(view, out var point) ? point : null;
     }
 
+    /// <summary>
+    /// Splits an enumerable of chartpoints by each null gap.
+    /// </summary>
+    /// <param name="points">The points.</param>
+    /// <param name="onDeleteNullPoint">Called when a point was deleted.</param>
+    /// <returns></returns>
+    public static IEnumerable<IEnumerable<ChartPoint>> SplitByNullGaps(
+        this IEnumerable<ChartPoint> points,
+        Action<ChartPoint> onDeleteNullPoint)
+    {
+        using var builder = new GapsBuilder(points.GetEnumerator());
+        while (!builder.Finished) yield return YieldReturnUntilNextNullChartPoint(builder, onDeleteNullPoint);
+    }
+
+    /// <summary>
+    /// Builds a enumerator with the necessary data to build an Spline.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    internal static IEnumerable<SplineData> AsSplineData(this IEnumerable<ChartPoint> source)
+    {
+        using var e = source.Where(x => !x.IsEmpty).GetEnumerator();
+
+        if (!e.MoveNext()) yield break;
+        var data = new SplineData(e.Current);
+
+        if (!e.MoveNext())
+        {
+            yield return data;
+            yield break;
+        }
+
+        data.GoNext(e.Current);
+
+        while (e.MoveNext())
+        {
+            yield return data;
+            data.IsFirst = false;
+            data.GoNext(e.Current);
+        }
+
+        data.IsFirst = false;
+        yield return data;
+
+        data.GoNext(data.Next);
+        yield return data;
+    }
+
+    private static IEnumerable<ChartPoint> YieldReturnUntilNextNullChartPoint(
+        GapsBuilder builder,
+        Action<ChartPoint> onDeleteNullPoint)
+    {
+        while (builder.Enumerator.MoveNext())
+        {
+            if (builder.Enumerator.Current.IsEmpty)
+            {
+                var wasEmpty = builder.IsEmpty;
+                builder.IsEmpty = true;
+                onDeleteNullPoint(builder.Enumerator.Current);
+                if (!wasEmpty) yield break; // if there are no points then do not return an empty enumerable...
+            }
+            else
+            {
+                yield return builder.Enumerator.Current;
+                builder.IsEmpty = false;
+            }
+        }
+
+        builder.Finished = true;
+    }
+
+    private class GapsBuilder : IDisposable
+    {
+        public GapsBuilder(IEnumerator<ChartPoint> enumerator)
+        {
+            Enumerator = enumerator;
+        }
+
+        public IEnumerator<ChartPoint> Enumerator { get; }
+
+        public bool IsEmpty { get; set; } = true;
+
+        public bool Finished { get; set; } = false;
+
+        public void Dispose()
+        {
+            Enumerator.Dispose();
+        }
+    }
+
+    internal class SplineData
+    {
+        public SplineData(ChartPoint start)
+        {
+            Previous = start;
+            Current = start;
+            Next = start;
+            AfterNext = start;
+        }
+
+        public ChartPoint Previous { get; set; }
+
+        public ChartPoint Current { get; set; }
+
+        public ChartPoint Next { get; set; }
+
+        public ChartPoint AfterNext { get; set; }
+
+        public bool IsFirst { get; set; } = true;
+
+        public void GoNext(ChartPoint point)
+        {
+            Previous = Current;
+            Current = Next;
+            Next = AfterNext;
+            AfterNext = point;
+        }
+    }
+
     private static ChartPoint? _findClosestTo(this IEnumerable<ChartPoint> points, LvcPoint point)
     {
         var o = points.Select(p => new { distance = p.DistanceTo(point), point = p }).OrderBy(p => p.distance).ToArray();
