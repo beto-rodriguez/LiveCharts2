e994d58dcad8d536b10cf9188b7e0b53bd3c8554
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2022-12-23 15:05:04 -0600

    compile msbuild

--- buildViews.ps1
+++ buildViews.ps1
@@ -78,10 +78,7 @@ function Add-Build {
     }
 
     if ($project.useMsbuild) {
-        # skip the build, it will be built in the pack method.
-        if ($project.packingMethod -ne "msbuild") {            
-            & $msbuild $project.src /p:configuration=$configuration /restore
-        }
+        & $msbuild $project.src /p:configuration=$configuration /restore
     }
     else {
         dotnet build $project.src -c $configuration
