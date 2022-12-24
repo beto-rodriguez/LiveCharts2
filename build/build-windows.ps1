d0a601f777df1e71d78b6c8d9d670fd7d1b82094
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2022-12-23 18:59:31 -0600

    this too

--- build/build-windows.ps1
+++ build/build-windows.ps1
@@ -17,8 +17,8 @@ $msbuild = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.
 
 dotnet workload install macos
 dotnet workload install ios
-dotnet workload install net6.0-maccatalyst
-dotnet workload install net6.0-android
+dotnet workload install maccatalyst 
+dotnet workload install android
 
 & $msbuild `
     ./src/skiasharp/LiveChartsCore.SkiaSharpView.Uno.WinUI/LiveChartsCore.SkiaSharpView.Uno.WinUI.csproj `
