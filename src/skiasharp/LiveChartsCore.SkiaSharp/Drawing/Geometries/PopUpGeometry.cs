53b9d0cd2a84ae175cf787cde5ee1ced0faccd44
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-05-26 14:43:25 -0600

    also border radius

--- src/skiasharp/LiveChartsCore.SkiaSharp/Drawing/Geometries/PopUpGeometry.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharp/Drawing/Geometries/PopUpGeometry.cs
@@ -33,7 +33,12 @@ public class PopUpGeometry : SizedGeometry
     /// <summary>
     /// Gets or sets the wedge size.
     /// </summary>
-    public int Wedge { get; set; } = 15;
+    public double Wedge { get; set; } = 15;
+
+    /// <summary>
+    /// Gets or sets the border radius.
+    /// </summary>
+    public double BorderRadius { get; set; } = 15;
 
     /// <summary>
     /// Gets or sets the placement.
@@ -44,14 +49,15 @@ public class PopUpGeometry : SizedGeometry
     {
         using var path = new SKPath();
 
-        var wedge = Wedge;
+        var wedge = (float)Wedge;
+        var br = (float)BorderRadius;
         var wf = 1.25f;
         var x = X + (Placement == PopUpPlacement.Right ? 1 : 0) * wedge;
         var y = Y + (Placement == PopUpPlacement.Bottom ? 1 : 0) * wedge;
         var w = Width - (Placement is PopUpPlacement.Right or PopUpPlacement.Left ? 1 : 0) * wedge;
         var h = Height - (Placement is PopUpPlacement.Bottom or PopUpPlacement.Top ? 1 : 0) * wedge;
 
-        path.MoveTo(x, y);
+        path.MoveTo(x + br, y);
 
         if (Placement == PopUpPlacement.Bottom)
         {
@@ -60,7 +66,8 @@ public class PopUpGeometry : SizedGeometry
             path.LineTo(x + (w * 0.5f + wedge * wf * 0.5f), y);
         }
 
-        path.LineTo(x + w, y);
+        path.LineTo(x + w - br, y);
+        path.ArcTo(new SKRect(x + w - br, y, x + w, y + br), 270, 90, false);
 
         if (Placement == PopUpPlacement.Left)
         {
@@ -69,7 +76,8 @@ public class PopUpGeometry : SizedGeometry
             path.LineTo(x + w, y + (h * 0.5f + wedge * wf * 0.5f));
         }
 
-        path.LineTo(x + w, y + h);
+        path.LineTo(x + w, y + h - br);
+        path.ArcTo(new SKRect(x + w - br, y + h - br, x + w, y + h), 0, 90, false);
 
         if (Placement == PopUpPlacement.Top)
         {
@@ -78,7 +86,8 @@ public class PopUpGeometry : SizedGeometry
             path.LineTo(x + (w * 0.5f - wedge * wf * 0.5f), y + h);
         }
 
-        path.LineTo(x, y + h);
+        path.LineTo(x + br, y + h);
+        path.ArcTo(new SKRect(x, y + h - br, x + br, y + h), 90, 90, false);
 
         if (Placement == PopUpPlacement.Right)
         {
@@ -86,6 +95,9 @@ public class PopUpGeometry : SizedGeometry
             path.LineTo(x - wedge, y + h * 0.5f);
             path.LineTo(x, y + (h * 0.5f - wedge * wf * 0.5f));
         }
+
+        path.LineTo(x, y + br);
+        path.ArcTo(new SKRect(x, y, x + br, y + br), 180, 90, false);
 
         path.Close();
         context.Canvas.DrawPath(path, context.Paint);
