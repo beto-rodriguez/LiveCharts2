ed2eef64a4ded0f1e5b739ec67ae14378dacee3f
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-06-04 13:27:37 -0600

    unused using

--- src/skiasharp/LiveChartsCore.SkiaSharp/SVGPoints.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharp/SVGPoints.cs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
 using SkiaSharp;
 
 namespace LiveChartsCore.SkiaSharpView;
