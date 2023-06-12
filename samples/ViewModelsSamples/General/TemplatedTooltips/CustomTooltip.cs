c36864ccfc0ec76833b3788191dfaf01141ec580
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-06-12 12:35:48 -0600

    indentation

--- samples/ViewModelsSamples/General/TemplatedTooltips/CustomTooltip.cs
+++ samples/ViewModelsSamples/General/TemplatedTooltips/CustomTooltip.cs
@@ -34,9 +34,10 @@ public class CustomTooltip : IChartTooltip<SkiaSharpDrawingContext>
             };
 
             _stackPanel
-                .Animate(new Animation(EasingFunctions.BounceOut, TimeSpan.FromSeconds(1)),
-                nameof(_stackPanel.X),
-                nameof(_stackPanel.Y));
+                .Animate(
+                    new Animation(EasingFunctions.BounceOut, TimeSpan.FromSeconds(1)),
+                    nameof(_stackPanel.X),
+                    nameof(_stackPanel.Y));
         }
 
         // clear the previous elements.
