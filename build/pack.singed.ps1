7a7d2f67099da7fcfbc419e58ef83bca24f8b1a1
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-06-13 09:45:23 -0600

    remove release pack

--- build/pack.singed.ps1
+++
@@ -1,7 +0,0 @@
-& $PSScriptRoot\pack.ps1 Release
-
-& dotnet nuget push **/*.nupkg --api-key oy2o6gainuded3gc42sq2gddywqhhhiu3cxtns4lokf5hu --source https://api.nuget.org/v3/index.json --skip-duplicate
-
-## this should only be ran once?
-## dotnet nuget add source --username beto-rodriguez --password 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 --store-password-in-clear-text --name github "https://nuget.pkg.github.com/beto-rodriguez/index.json"
-& dotnet nuget push **/*.nupkg --api-key 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 --source "github" --skip-duplicate
