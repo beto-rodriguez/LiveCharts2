79f9d1318887105fe0dcf237ba9fc87a1848f31e
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-06-16 20:40:24 -0600

    improve non-latin sample, improves #1068

--- samples/ViewModelsSamples/Axes/LabelsFormat2/ViewModel.cs
+++ samples/ViewModelsSamples/Axes/LabelsFormat2/ViewModel.cs
@@ -8,6 +8,19 @@ namespace ViewModelsSamples.Axes.LabelsFormat2;
 
 public partial class ViewModel : ObservableObject
 {
+    public ViewModel()
+    {
+        // you need to enable the Chinese characters for SkiaSharp
+        // use the SKFontManager.Default.MatchCharacter() SkiaSharp function.
+
+        // it is recommended to run this only once when the app starts.
+        LiveChartsSkiaSharp.DefaultSKTypeface = SKFontManager.Default.MatchCharacter('汉');
+
+        // SKTypeface = SKFontManager.Default.MatchCharacter('أ'), // Arab
+        // SKTypeface = SKFontManager.Default.MatchCharacter('あ'), // Japanese
+        // SKTypeface = SKFontManager.Default.MatchCharacter('Ж'), // Russian
+    }
+
     public ISeries[] Series { get; set; } =
     {
         new ColumnSeries<double> { Values = new double[] { 426, 583, 104 } },
@@ -20,22 +33,7 @@ public partial class ViewModel : ObservableObject
         {
             Name = "Salesman/woman",
             Labels = new string[] { "王", "赵", "张" },
-            LabelsPaint = new SolidColorPaint
-            {
-                Color = SKColors.Black,
-
-                // you need to enable the Chinese characters for SkiaSharp
-                // use the SKFontManager.Default.MatchCharacter() SkiaSharp function.
-
-                SKTypeface = SKFontManager.Default.MatchCharacter('汉') // 汉语 // mark
-                // SKTypeface = SKFontManager.Default.MatchCharacter('أ'), // Arab
-                // SKTypeface = SKFontManager.Default.MatchCharacter('あ'), // Japanese
-                // SKTypeface = SKFontManager.Default.MatchCharacter('Ж'), // Russian
-
-                // You can also register a default global SKTypeface // mark
-                // this will load the font in any Paint when the SKTypeface property is null. // mark
-                // for more info see: ToDo: Add link!!! // mark
-            }
+            LabelsPaint = new SolidColorPaint(SKColors.Black)
         }
     };
 
