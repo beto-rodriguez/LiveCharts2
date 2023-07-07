de3eb4ab6c35dddb5539cb0b679c3dfaeb8a6f17
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-07 16:53:15 -0600

    scrollable chart sample in wpf

---
+++ samples/ViewModelsSamples/General/Scrollable/ViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+
+namespace ViewModelsSamples.General.Scrollable;
+
+public partial class ViewModel
+{
+    private readonly ObservableCollection<ObservablePoint> _values = new();
+
+    public ViewModel()
+    {
+        var trend = 1000;
+        var r = new Random();
+
+        for (var i = 0; i < 500; i++)
+        {
+            _values.Add(new ObservablePoint(i, trend += r.Next(-20, 20)));
+        }
+
+        Series = new ISeries[]
+        {
+            new LineSeries<ObservablePoint>
+            {
+                Values = _values,
+                GeometryStroke = null,
+                GeometryFill = null,
+                DataPadding = new(0, 1)
+            }
+        };
+
+        ScrollbarSeries = new ISeries[]
+        {
+            new LineSeries<ObservablePoint>
+            {
+                Values = _values,
+                GeometryStroke = null,
+                GeometryFill = null,
+                DataPadding = new(0, 1)
+            }
+        };
+
+        ScrollableAxes = new Axis[]
+        {
+            new Axis()
+        };
+
+        InvisibleX = new[] { new Axis { IsVisible = false } };
+        InvisibleY = new[] { new Axis { IsVisible = false } };
+
+        // force the left margin to be 100 in both charts, this will
+        // align the start point ogg the series, no matter the size
+        // of the labels in the Y axis of both chart.
+        var auto = LiveChartsCore.Measure.Margin.Auto;
+        Margin = new(100, auto, auto, auto);
+    }
+
+    public ISeries[] Series { get; set; } =
+    {
+        new ScatterSeries<ObservablePoint>
+        {
+            Values = new ObservableCollection<ObservablePoint>
+            {
+                new(2.2, 5.4),
+                new(4.5, 2.5),
+                new(4.2, 7.4),
+                new(6.4, 9.9),
+                new(4.2, 9.2),
+                new(5.8, 3.5),
+                new(7.3, 5.8),
+                new(8.9, 3.9),
+                new(6.1, 4.6),
+                new(9.4, 7.7),
+                new(8.4, 8.5),
+                new(3.6, 9.6),
+                new(4.4, 6.3),
+                new(5.8, 4.8),
+                new(6.9, 3.4),
+                new(7.6, 1.8),
+                new(8.3, 8.3),
+                new(9.9, 5.2),
+                new(8.1, 4.7),
+                new(7.4, 3.9),
+                new(6.8, 2.3),
+                new(5.3, 7.1),
+            }
+        }
+    };
+
+    public Axis[] ScrollableAxes { get; set; }
+
+    public ISeries[] ScrollbarSeries { get; set; }
+    public Axis[] InvisibleX { get; set; }
+    public Axis[] InvisibleY { get; set; }
+    public LiveChartsCore.Measure.Margin Margin { get; set; }
+
+    public RectangularSection[] Thumbs { get; set; } =
+    {
+        new RectangularSection
+        {
+            // creates a section from 3 to 4 in the X axis
+            Xi = 3,
+            Xj = 4,
+            Fill = new SolidColorPaint(new SKColor(255, 205, 210))
+        },
+    };
+}
---
+++ samples/WPFSample/General/Scrollable/View.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore.SkiaSharpView.WPF;
+using ViewModelsSamples.General.Scrollable;
+
+namespace WPFSample.General.Scrollable;
+
+/// <summary>
+/// Interaction logic for View.xaml
+/// </summary>
+public partial class View : UserControl
+{
+    private bool _isDown = true;
+
+    public View()
+    {
+        InitializeComponent();
+    }
+
+    private void OnChart_Updated(IChartView<SkiaSharpDrawingContext> chart)
+    {
+        var vm = (ViewModel)DataContext;
+        var cartesianChart = (CartesianChart)chart;
+
+        var x = cartesianChart.XAxes.First();
+
+        var thumb = vm.Thumbs[0];
+        thumb.Xi = x.MinLimit;
+        thumb.Xj = x.MaxLimit;
+    }
+
+    private void CartesianChart_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        _isDown = true;
+    }
+
+    private void CartesianChart_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (!_isDown) return;
+
+        var vm = (ViewModel)DataContext;
+        var scrollBarChart = (CartesianChart)FindName("ScrollBarChart");
+
+        var pointerPosition = e.GetPosition(scrollBarChart);
+        var positionInData = scrollBarChart.ScalePixelsToData(new(pointerPosition.X, pointerPosition.Y));
+
+        var thumb = vm.Thumbs[0];
+        var currentRange = thumb.Xj - thumb.Xi;
+
+        thumb.Xi = positionInData.X - currentRange / 2;
+        thumb.Xj = positionInData.X + currentRange / 2;
+
+        vm.ScrollableAxes[0].MinLimit = thumb.Xi;
+        vm.ScrollableAxes[0].MaxLimit = thumb.Xj;
+    }
+
+    private void CartesianChart_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        _isDown = false;
+    }
+}
