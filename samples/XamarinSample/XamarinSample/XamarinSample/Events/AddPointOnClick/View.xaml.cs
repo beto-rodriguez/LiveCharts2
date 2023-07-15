a61da1a2f4a5d1695d6f7ef8590350c1367cb19a
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-15 17:35:09 -0600

    use tap command in xamarin

--- samples/XamarinSample/XamarinSample/XamarinSample/Events/AddPointOnClick/View.xaml.cs
+++ samples/XamarinSample/XamarinSample/XamarinSample/Events/AddPointOnClick/View.xaml.cs
@@ -1,8 +1,4 @@
-﻿using LiveChartsCore.Defaults;
-using LiveChartsCore.Drawing;
-using LiveChartsCore.SkiaSharpView.Xamarin.Forms;
-using ViewModelsSamples.Events.AddPointOnClick;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XamarinSample.Events.AddPointOnClick;
@@ -13,22 +9,5 @@ public partial class View : ContentPage
     public View()
     {
         InitializeComponent();
-    }
-
-    private void Chart_Touched(object sender, SkiaSharp.Views.Forms.SKTouchEventArgs e)
-    {
-        var chart = (CartesianChart)FindByName("chart");
-
-        var viewModel = (ViewModel)BindingContext;
-
-        // scales the UI coordinates to the corresponding data in the chart.
-        var dataCoordinates = chart.ScalePixelsToData(new LvcPointD(e.Location.X, e.Location.Y));
-
-        // finally add the new point to the data in our chart.
-        viewModel?.Data.Add(new ObservablePoint(dataCoordinates.X, dataCoordinates.Y));
-
-        // You can also get all the points or visual elements in a given location.
-        var points = chart.GetPointsAt(new LvcPoint((float)e.Location.X, (float)e.Location.Y));
-        var visuals = chart.GetVisualsAt(new LvcPoint((float)e.Location.X, (float)e.Location.Y));
     }
 }
