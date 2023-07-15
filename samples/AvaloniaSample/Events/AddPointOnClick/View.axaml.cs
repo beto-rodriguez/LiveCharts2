cbf2f7c2f2f86412b5e497b8bae2daeb248f2191
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-14 22:32:31 -0600

    update avalonia samples to commands

--- samples/AvaloniaSample/Events/AddPointOnClick/View.axaml.cs
+++ samples/AvaloniaSample/Events/AddPointOnClick/View.axaml.cs
@@ -1,9 +1,5 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using LiveChartsCore.Defaults;
-using LiveChartsCore.Drawing;
-using LiveChartsCore.SkiaSharpView.Avalonia;
-using ViewModelsSamples.Events.AddPointOnClick;
 
 namespace AvaloniaSample.Events.AddPointOnClick;
 
@@ -12,25 +8,6 @@ public class View : UserControl
     public View()
     {
         InitializeComponent();
-    }
-
-    private void ChartPointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
-    {
-        var chart = this.FindControl<CartesianChart>("chart");
-        var viewModel = DataContext as ViewModel;
-
-        // gets the point in the UI coordinates.
-        var p = e.GetPosition(chart);
-
-        // scales the UI coordinates to the corresponding data in the chart.
-        var dataCoordinates = chart.ScalePixelsToData(new LvcPointD(p.X, p.Y));
-
-        // finally add the new point to the data in our chart.
-        viewModel?.Data.Add(new ObservablePoint(dataCoordinates.X, dataCoordinates.Y));
-
-        // You can also get all the points or visual elements in a given location.
-        var points = chart.GetPointsAt(new LvcPoint(p.X, p.Y));
-        var visuals = chart.GetVisualsAt(new LvcPoint(p.X, p.Y));
     }
 
     private void InitializeComponent()
