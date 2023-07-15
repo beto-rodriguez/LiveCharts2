debaa3168e96991c46730ebe13f6d43011767bc9
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-15 17:58:05 -0600

    fixes #1113

--- src/skiasharp/LiveChartsCore.SkiaSharp/SKCharts/SKDefaultTooltip.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharp/SKCharts/SKDefaultTooltip.cs
@@ -84,6 +84,10 @@ public class SKDefaultTooltip : IChartTooltip<SkiaSharpDrawingContext>
     {
         const int wedge = 13;
 
+        if (chart.View.LegendTextSize is not null) TextSize = chart.View.LegendTextSize.Value;
+        if (chart.View.TooltipBackgroundPaint is not null) BackgroundPaint = chart.View.TooltipBackgroundPaint;
+        if (chart.View.TooltipTextPaint is not null) FontPaint = chart.View.TooltipTextPaint;
+
         if (_panel is null)
         {
             _panel = new StackPanel<PopUpGeometry, SkiaSharpDrawingContext>
@@ -102,10 +106,6 @@ public class SKDefaultTooltip : IChartTooltip<SkiaSharpDrawingContext>
                     nameof(RoundedRectangleGeometry.X),
                     nameof(RoundedRectangleGeometry.Y));
         }
-
-        if (chart.View.LegendTextSize is not null) TextSize = chart.View.LegendTextSize.Value;
-        if (chart.View.TooltipBackgroundPaint is not null) BackgroundPaint = chart.View.TooltipBackgroundPaint;
-        if (chart.View.TooltipTextPaint is not null) FontPaint = chart.View.TooltipTextPaint;
 
         if (BackgroundPaint is not null) BackgroundPaint.ZIndex = s_zIndex;
         if (FontPaint is not null) FontPaint.ZIndex = s_zIndex + 1;
