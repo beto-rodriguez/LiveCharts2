d81a89a50f2f0f3d192ce55c8fdc4fef4c8c2edc
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-14 18:06:16 -0600

    update addpoint on click to use commands

--- samples/WPFSample/Events/AddPointOnClick/View.xaml.cs
+++ samples/WPFSample/Events/AddPointOnClick/View.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System.Windows.Controls;
-using LiveChartsCore.Defaults;
-using LiveChartsCore.Drawing;
-using LiveChartsCore.SkiaSharpView.WPF;
-using ViewModelsSamples.Events.AddPointOnClick;
 
 namespace WPFSample.Events.AddPointOnClick;
 
@@ -14,24 +10,5 @@ public partial class View : UserControl
     public View()
     {
         InitializeComponent();
-    }
-
-    private void Chart_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-    {
-        var chart = (CartesianChart)FindName("chart");
-        var viewModel = (ViewModel)DataContext;
-
-        // gets the point in the UI coordinates.
-        var p = e.GetPosition(chart);
-
-        // scales the UI coordinates to the corresponding data in the chart.
-        var scaledPoint = chart.ScalePixelsToData(new LvcPointD(p.X, p.Y));
-
-        // finally add the new point to the data in our chart.
-        viewModel.Data.Add(new ObservablePoint(scaledPoint.X, scaledPoint.Y));
-
-        // You can also get all the points or visual elements in a given location.
-        var points = chart.GetPointsAt(new LvcPoint((float)p.X, (float)p.Y));
-        var visuals = chart.GetVisualsAt(new LvcPoint((float)p.X, (float)p.Y));
     }
 }
