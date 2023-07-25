9368c881e84a0bae5a8d9c7e3dcb0cb7baf18232
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-25 09:03:33 -0600

    update samples

--- samples/ViewModelsSamples/Lines/Custom/ViewModel.cs
+++ samples/ViewModelsSamples/Lines/Custom/ViewModel.cs
@@ -3,67 +3,46 @@ using LiveChartsCore;
 using LiveChartsCore.Drawing;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
-using LiveChartsCore.SkiaSharpView.Painting;
-using SkiaSharp;
 
 namespace ViewModelsSamples.Lines.Custom;
 
 public partial class ViewModel : ObservableObject
 {
-    public ViewModel()
-    {
-        Series = new ISeries[]
+    public ISeries[] Series { get; set; } =
         {
             new LineSeries<double>
             {
-                Values = new double[] { 3, 1, 4, 3, 2, -5, -2 },
-                GeometrySize = 10,
-                Fill = null
+                Values = new double[] { 2, 1, 4, 2, 2, -5, -2 },
+                Fill = null,
+                GeometrySize = 20
             },
 
-            // use the second argument type to specify the geometry to draw for every point
+            // use the second type parameter to specify the geometry to draw for every point
             // there are already many predefined geometries in the
             // LiveChartsCore.SkiaSharpView.Drawing.Geometries namespace
             new LineSeries<double, RectangleGeometry>
             {
                 Values = new double[] { 3, 3, -3, -2, -4, -3, -1 },
-                Fill = null
+                Fill = null,
+                GeometrySize = 20
             },
 
-            // You can also use a SVG path data to draw the geometry
+            // You can also use SVG paths to draw the geometry
+            // LiveCharts already provides some predefined paths in the SVGPoints class.
             new LineSeries<double, SVGPathGeometry>
             {
                 Values = new double[] { -2, 2, 1, 3, -1, 4, 3 },
-                Stroke = new SolidColorPaint(SKColors.DarkOliveGreen, 3),
                 Fill = null,
                 GeometrySvg = SVGPoints.Star,
-                GeometryStroke = new SolidColorPaint(SKColors.DarkOliveGreen, 3),
-                GeometryFill = new SolidColorPaint(SKColors.White),
-                GeometrySize = 35
+                GeometrySize = 20
             },
 
-            new LineSeries<double, SVGPathGeometry>
+            // you can declare your own gemetry and use the SkiaSharp api to draw it
+            new LineSeries<double, MyGeometry>
             {
-                Values = new double[] { 3, 5, 2, 4, 3, 2, 1 },
-                Stroke = new SolidColorPaint(SKColors.DarkOliveGreen, 3),
+                Values = new double[] { 4, 5, 2, 4, 3, 2, 1 },
                 Fill = null,
-                GeometrySvg = SVGPoints.Pin,
-                GeometryStroke = new SolidColorPaint(SKColors.DarkOliveGreen, 3),
-                GeometryFill = new SolidColorPaint(SKColors.White),
-                GeometrySize = 35
+                GeometrySize = 20
             },
         };
-
-        var s = (LineSeries<double, SVGPathGeometry>)Series[2];
-
-        s.ChartPointPointerDown += (chart, point) =>
-        {
-            if (point is null) return;
-            // you can change the svg at runtime
-            point.Context.Series.GeometrySvg =
-                s.GeometrySvg == SVGPoints.Star ? SVGPoints.Gem : SVGPoints.Star;
-        };
-    }
-
-    public ISeries[] Series { get; set; }
 }
