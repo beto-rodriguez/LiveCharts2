b17439393a4a928011724498df586c07829f4078
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-07-26 13:54:54 -0600

    make basic pie sample actually basic.

--- samples/ViewModelsSamples/Pies/Basic/ViewModel.cs
+++ samples/ViewModelsSamples/Pies/Basic/ViewModel.cs
@@ -10,37 +10,8 @@ namespace ViewModelsSamples.Pies.Basic;
 
 public partial class ViewModel : ObservableObject
 {
-    public ViewModel()
-    {
-        // you could convert any IEnumerable to a pie series collection
-        var data = new double[] { 2, 4, 1, 4, 3 };
-
-        // Series = data.AsLiveChartsPieSeries(); this could be enough in some cases // mark
-
-        // but you can customize the series properties using the following overload: // mark
-        Series = data.AsPieSeries((value, series) =>
-        {
-            // here you can configure the series assigned to each value.
-            series.Name = $"Series for value {value}";
-            series.DataLabelsPaint = new SolidColorPaint(new SKColor(30, 30, 30));
-            series.DataLabelsPosition = LiveChartsCore.Measure.PolarLabelsPosition.Outer;
-            series.DataLabelsFormatter =
-                p => $"{p.Coordinate.PrimaryValue} / {p.StackedValue!.Total} ({p.StackedValue.Share:P2})";
-            series.ToolTipLabelFormatter = p => $"{p.Coordinate.PrimaryValue:C2}";
-        });
-
-        // this is an equivalent and more verbose syntax. // mark
-        // Series = new ISeries[]
-        // {
-        //     new PieSeries<double> { Values = new double[] { 2 }, Name = "Slice 1" },
-        //     new PieSeries<double> { Values = new double[] { 4 }, Name = "Slice 2" },
-        //     new PieSeries<double> { Values = new double[] { 1 }, Name = "Slice 3" },
-        //     new PieSeries<double> { Values = new double[] { 4 }, Name = "Slice 4" },
-        //     new PieSeries<double> { Values = new double[] { 3 }, Name = "Slice 5" }
-        // };
-    }
-
-    public IEnumerable<ISeries> Series { get; set; }
+    public IEnumerable<ISeries> Series { get; set; } =
+        new[] { 2, 4, 1, 4, 3 }.AsPieSeries();
 
     public LabelVisual Title { get; set; } =
         new LabelVisual
