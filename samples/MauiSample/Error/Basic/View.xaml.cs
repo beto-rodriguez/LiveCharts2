49bd7a21177432221ccda49c414f49d3fab5ba14
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-08-17 18:27:03 -0600

    add samples

---
+++ samples/MauiSample/Error/Basic/View.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSample.Error.Basic;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class View : ContentPage
+{
+    public View()
+    {
+        InitializeComponent();
+    }
+}
