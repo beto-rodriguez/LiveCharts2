d869787664e2bc323b6cdf8a1b30deeb713d1c0f
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2024-10-26 09:22:34 -0600

    remmove unused ns

--- src/skiasharp/LiveChartsCore.SkiaSharp/Painting/SolidColorPaint.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharp/Painting/SolidColorPaint.cs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System.Buffers.Text;
 using LiveChartsCore.Drawing;
 using LiveChartsCore.Motion;
 using LiveChartsCore.SkiaSharpView.Drawing;
