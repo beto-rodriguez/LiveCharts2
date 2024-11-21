a72cf18f5db90a4cfef26e5e114308bba031b88c
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2024-11-21 13:56:31 -0600

    Update namespace in ErrorVisual.cs
    Changed the namespace of ErrorVisual.cs from LiveChartsCore.Drawing to LiveChartsCore.Drawing.Segments. This reorganization helps in better categorizing and modularizing the components within the project.

--- src/LiveChartsCore/Drawing/Segments/ErrorVisual.cs
+++ src/LiveChartsCore/Drawing/Segments/ErrorVisual.cs
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-namespace LiveChartsCore.Drawing;
+namespace LiveChartsCore.Drawing.Segments;
 
 /// <summary>
 /// Defines an error visual.
