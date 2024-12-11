b0f3ad1227581e1d7a27f4acd1d1fcf432d1dd54
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2024-12-10 22:01:07 -0600

    update table layout test

--- tests/LiveChartsCore.UnitTesting/LayoutTests/TableLayoutTest.cs
+++ tests/LiveChartsCore.UnitTesting/LayoutTests/TableLayoutTest.cs
@@ -1,6 +1,7 @@
 ﻿using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Drawing;
 using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.SkiaSharpView.Drawing.Layouts;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.SKCharts;
 using LiveChartsCore.SkiaSharpView.VisualElements;
@@ -19,7 +20,7 @@ public class TableLayoutTest
         var px = 10;
         var py = 20;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -28,23 +29,23 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.Start
         };
 
-        GeometryVisual<RectangleGeometry> g1, g2;
+        RectangleGeometry g1, g2;
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 10,
             Height = 20,
             Fill = new SolidColorPaint(SKColors.Red)
         }, 0, 0);
 
-        table.AddChild(g1 = new GeometryVisual<RectangleGeometry>
+        table.AddChild(g1 = new RectangleGeometry
         {
             Width = 15,
             Height = 10,
             Fill = new SolidColorPaint(SKColors.Green)
         }, 0, 1);
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 20,
             Height = 30,
@@ -52,7 +53,7 @@ public class TableLayoutTest
         }, 1, 0);
 
         table.AddChild(
-            g2 = new GeometryVisual<RectangleGeometry>
+            g2 = new RectangleGeometry
             {
                 Width = 25,
                 Height = 20,
@@ -69,32 +70,26 @@ public class TableLayoutTest
                 {
                     Values = new double[] { 1, 2, 3 }
                 }
-            },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
             }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g1Geometry = (RectangleGeometry)g1.GetDrawnGeometries()[0];
-        var g2Geometry = (RectangleGeometry)g2.GetDrawnGeometries()[0];
-
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
             // 100 left + 10 padding + 20 column width
-            g1Geometry.X == 100 + px + 20 &&
+            g1.X == 100 + px + 20 &&
 
             // 200 top + 20 padding
-            g1Geometry.Y == 200 + py &&
+            g1.Y == 200 + py &&
 
             // 100 left + 10 padding + 20 column width
-            g2Geometry.X == 100 + px + 20 &&
+            g2.X == 100 + px + 20 &&
 
             // 200 top + 20 padding + 20 previous row height
-            g2Geometry.Y == 200 + py + 20 &&
+            g2.Y == 200 + py + 20 &&
 
             // 10 padding + columns width + 10 padding
             tableSize.Width == px + 20 + 25 + px &&
@@ -108,7 +103,7 @@ public class TableLayoutTest
         var px = 10;
         var py = 20;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -117,23 +112,23 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.Start
         };
 
-        GeometryVisual<RectangleGeometry> g1, g2;
+        RectangleGeometry g1, g2;
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 10,
             Height = 20,
             Fill = new SolidColorPaint(SKColors.Red)
         }, 1, 1);
 
-        table.AddChild(g1 = new GeometryVisual<RectangleGeometry>
+        table.AddChild(g1 = new RectangleGeometry
         {
             Width = 15,
             Height = 10,
             Fill = new SolidColorPaint(SKColors.Green)
         }, 1, 2);
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 20,
             Height = 30,
@@ -141,7 +136,7 @@ public class TableLayoutTest
         }, 2, 1);
 
         table.AddChild(
-            g2 = new GeometryVisual<RectangleGeometry>
+            g2 = new RectangleGeometry
             {
                 Width = 25,
                 Height = 20,
@@ -158,32 +153,26 @@ public class TableLayoutTest
                 {
                     Values = new double[] { 1, 2, 3 }
                 }
-            },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
             }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g1Geometry = (RectangleGeometry)g1.GetDrawnGeometries()[0];
-        var g2Geometry = (RectangleGeometry)g2.GetDrawnGeometries()[0];
-
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
             // 100 left + 10 padding + 20 column width
-            g1Geometry.X == 100 + px + 20 &&
+            g1.X == 100 + px + 20 &&
 
             // 200 top + 20 padding
-            g1Geometry.Y == 200 + py &&
+            g1.Y == 200 + py &&
 
             // 100 left + 10 padding + 20 column width
-            g2Geometry.X == 100 + px + 20 &&
+            g2.X == 100 + px + 20 &&
 
             // 200 top + 20 padding + 20 previous row height
-            g2Geometry.Y == 200 + py + 20 &&
+            g2.Y == 200 + py + 20 &&
 
             // 10 padding + columns width + 10 padding
             tableSize.Width == px + 20 + 25 + px &&
@@ -197,7 +186,7 @@ public class TableLayoutTest
         var px = 10;
         var py = 20;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -206,9 +195,9 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.Start
         };
 
-        GeometryVisual<RectangleGeometry> g1, g2;
+        RectangleGeometry g1, g2;
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 5,
             Height = 20,
@@ -217,7 +206,7 @@ public class TableLayoutTest
 
         // ignore the 0,1 cell
 
-        table.AddChild(g1 = new GeometryVisual<RectangleGeometry>
+        table.AddChild(g1 = new RectangleGeometry
         {
             Width = 15,
             Height = 10,
@@ -226,14 +215,14 @@ public class TableLayoutTest
 
         // also ignore the row 1
 
-        //table.AddChild(new GeometryVisual<RectangleGeometry>
+        //table.AddChild(new RectangleGeometry
         //{
         //    Width = 10,
         //    Height = 30,
         //    Fill = new SolidColorPaint(SKColors.Blue.WithAlpha(100))
         //}, 2, 0);
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 10,
             Height = 30,
@@ -241,7 +230,7 @@ public class TableLayoutTest
         }, 2, 1);
 
         table.AddChild(
-            g2 = new GeometryVisual<RectangleGeometry>
+            g2 = new RectangleGeometry
             {
                 Width = 25,
                 Height = 20,
@@ -258,32 +247,26 @@ public class TableLayoutTest
                 {
                     Values = new double[] { 1, 2, 3 }
                 }
-            },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
             }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g1Geometry = (RectangleGeometry)g1.GetDrawnGeometries()[0];
-        var g2Geometry = (RectangleGeometry)g2.GetDrawnGeometries()[0];
-
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
             // 100 left + 10 padding + 15 column width
-            g1Geometry.X == 100 + px + 15 &&
+            g1.X == 100 + px + 15 &&
 
             // 200 top + 20 padding
-            g1Geometry.Y == 200 + py &&
+            g1.Y == 200 + py &&
 
             // 100 left + 10 padding + 15 column width
-            g2Geometry.X == 100 + px + 15 &&
+            g2.X == 100 + px + 15 &&
 
             // 200 top + 20 padding + 20 previous row height
-            g2Geometry.Y == 200 + py + 20 &&
+            g2.Y == 200 + py + 20 &&
 
             // 10 padding + columns width + 10 padding
             tableSize.Width == px + 5 + 10 + 25 + px &&
@@ -297,7 +280,7 @@ public class TableLayoutTest
         var px = 10;
         var py = 20;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -306,23 +289,23 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.Middle
         };
 
-        GeometryVisual<RectangleGeometry> g1, g2;
+        RectangleGeometry g1, g2;
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 10,
             Height = 20,
             Fill = new SolidColorPaint(SKColors.Red)
         }, 0, 0);
 
-        table.AddChild(g1 = new GeometryVisual<RectangleGeometry>
+        table.AddChild(g1 = new RectangleGeometry
         {
             Width = 15,
             Height = 10,
             Fill = new SolidColorPaint(SKColors.Green)
         }, 0, 1);
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 20,
             Height = 30,
@@ -330,7 +313,7 @@ public class TableLayoutTest
         }, 1, 0);
 
         table.AddChild(
-            g2 = new GeometryVisual<RectangleGeometry>
+            g2 = new RectangleGeometry
             {
                 Width = 25,
                 Height = 20,
@@ -347,31 +330,25 @@ public class TableLayoutTest
                 {
                     Values = new double[] { 1, 2, 3 }
                 }
-            },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
             }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g1Geometry = (RectangleGeometry)g1.GetDrawnGeometries()[0];
-        var g2Geometry = (RectangleGeometry)g2.GetDrawnGeometries()[0];
-
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
-            g1Geometry.X == 135 &&
+            g1.X == 135 &&
 
             // 200 top + 20 padding
-            g1Geometry.Y == 225 &&
+            g1.Y == 225 &&
 
             // 100 left + 10 padding + 20 column width
-            g2Geometry.X == 130 &&
+            g2.X == 130 &&
 
             // 200 top + 20 padding + 20 previous row height
-            g2Geometry.Y == 245 &&
+            g2.Y == 245 &&
 
             // 10 padding + columns width + 10 padding
             tableSize.Width == px + 20 + 25 + px &&
@@ -385,7 +362,7 @@ public class TableLayoutTest
         var px = 10;
         var py = 20;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -394,23 +371,23 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.End
         };
 
-        GeometryVisual<RectangleGeometry> g1, g2;
+        RectangleGeometry g1, g2;
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 10,
             Height = 20,
             Fill = new SolidColorPaint(SKColors.Red)
         }, 0, 0);
 
-        table.AddChild(g1 = new GeometryVisual<RectangleGeometry>
+        table.AddChild(g1 = new RectangleGeometry
         {
             Width = 15,
             Height = 10,
             Fill = new SolidColorPaint(SKColors.Green)
         }, 0, 1);
 
-        table.AddChild(new GeometryVisual<RectangleGeometry>
+        table.AddChild(new RectangleGeometry
         {
             Width = 20,
             Height = 30,
@@ -418,7 +395,7 @@ public class TableLayoutTest
         }, 1, 0);
 
         table.AddChild(
-            g2 = new GeometryVisual<RectangleGeometry>
+            g2 = new RectangleGeometry
             {
                 Width = 25,
                 Height = 20,
@@ -435,31 +412,25 @@ public class TableLayoutTest
                 {
                     Values = new double[] { 1, 2, 3 }
                 }
-            },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
             }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g1Geometry = (RectangleGeometry)g1.GetDrawnGeometries()[0];
-        var g2Geometry = (RectangleGeometry)g2.GetDrawnGeometries()[0];
-
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
-            g1Geometry.X == 140 &&
+            g1.X == 140 &&
 
             // 200 top + 20 padding
-            g1Geometry.Y == 230 &&
+            g1.Y == 230 &&
 
             // 100 left + 10 padding + 20 column width
-            g2Geometry.X == 130 &&
+            g2.X == 130 &&
 
             // 200 top + 20 padding + 20 previous row height
-            g2Geometry.Y == 250 &&
+            g2.Y == 250 &&
 
             // 10 padding + columns width + 10 padding
             tableSize.Width == px + 20 + 25 + px &&
@@ -473,7 +444,7 @@ public class TableLayoutTest
         var px = 0;
         var py = 0;
 
-        var table = new TableLayout<RectangleGeometry>
+        var table = new TableLayout
         {
             Padding = new Drawing.Padding(px, py),
             X = 100,
@@ -482,27 +453,27 @@ public class TableLayoutTest
             HorizontalAlignment = Drawing.Align.Start
         };
 
-        LabelVisual g00, g01, g10, g11;
+        LabelGeometry g00, g01, g10, g11;
 
-        table.AddChild(g00 = new LabelVisual
+        table.AddChild(g00 = new LabelGeometry
         {
             Text = "xxxx",
             Paint = new SolidColorPaint(SKColors.Red)
         }, 0, 0);
 
-        table.AddChild(g01 = new LabelVisual
+        table.AddChild(g01 = new LabelGeometry
         {
             Text = "xxxxxxxxxxxxxxxxxxxxxxxx",
             Paint = new SolidColorPaint(SKColors.Green)
         }, 0, 1);
 
-        table.AddChild(g10 = new LabelVisual
+        table.AddChild(g10 = new LabelGeometry
         {
             Text = "xxxxxxxxxxxxxxxxxxxxxxxx",
             Paint = new SolidColorPaint(SKColors.Yellow)
         }, 1, 0);
 
-        table.AddChild(g11 = new LabelVisual
+        table.AddChild(g11 = new LabelGeometry
         {
             Text = "xxxx",
             Paint = new SolidColorPaint(SKColors.Blue)
@@ -519,33 +490,25 @@ public class TableLayoutTest
                     Values = new double[] { 1, 2, 3 }
                 }
             },
-            VisualElements = new CoreVisualElement[]
-            {
-                table
-            }
         };
 
+        chart.CoreCanvas.AddGeometry(table);
         _ = chart.GetImage();
 
-        var g00g = (LabelGeometry)g00.GetDrawnGeometries()[0];
-        var g01g = (LabelGeometry)g01.GetDrawnGeometries()[0];
-        var g10g = (LabelGeometry)g10.GetDrawnGeometries()[0];
-        var g11g = (LabelGeometry)g11.GetDrawnGeometries()[0];
+        g00.Paint = g00.Paint;
+        g01.Paint = g01.Paint;
+        g10.Paint = g10.Paint;
+        g11.Paint = g11.Paint;
 
-        g00g.Paint = g00.Paint;
-        g01g.Paint = g01.Paint;
-        g10g.Paint = g10.Paint;
-        g11g.Paint = g11.Paint;
-
-        var g00m = g00g.Measure();
-        var g01m = g01g.Measure();
-        var g10m = g10g.Measure();
-        var g11m = g11g.Measure();
+        var g00m = g00.Measure();
+        var g01m = g01.Measure();
+        var g10m = g10.Measure();
+        var g11m = g11.Measure();
 
         var wc0 = g00m.Width > g01m.Width ? g00m.Width : g10m.Width;
         var wc1 = g10m.Width > g11m.Width ? g01m.Width : g11m.Width;
 
-        var tableSize = table.Measure((Chart)chart.Core);
+        var tableSize = table.Measure();
 
         Assert.IsTrue(
             // 10 padding + columns width + 10 padding
