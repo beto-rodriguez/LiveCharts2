0876d7596c5967b6744d8cf960e426c75735a5c1
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-01-02 21:33:38 -0600

    remove workaround to fix an issue caused by the previous commit...

--- src/LiveChartsCore/CoreBoxSeries.cs
+++ src/LiveChartsCore/CoreBoxSeries.cs
@@ -329,14 +329,6 @@ public abstract class CoreBoxSeries<TModel, TVisual, TLabel, TMiniatureGeometry>
         var ts = tickSecondary.Value * DataPadding.X;
         var tp = tickPrimary.Value * DataPadding.Y;
 
-        // using different methods for both primary and secondary axis seems to be the best solution
-        // if this the following 2 lines needs to be changed again, please ensure that the following test passes:
-        // https://github.com/beto-rodriguez/LiveCharts2/issues/522
-        // https://github.com/beto-rodriguez/LiveCharts2/issues/642
-
-        if (rawBaseBounds.VisibleSecondaryBounds.Delta == 0) ts = secondaryAxis.UnitWidth * DataPadding.X;
-        if (rawBaseBounds.VisiblePrimaryBounds.Delta == 0) tp = rawBaseBounds.VisiblePrimaryBounds.Max * 0.25f;
-
         var rgs = GetRequestedGeometrySize();
         var rso = GetRequestedSecondaryOffset();
         var rpo = GetRequestedPrimaryOffset();
