6ac4daa7c2fc945e67be926fd0281425bf3b0c2e
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-01-11 20:32:46 -0600

    match scale sample

---
+++ samples/UnoPlatform_v5_2_175/UnoPlatform_v5_2_175/LiveChartsSamples/Axes/MatchScale/View.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace UnoWinUISample.Axes.MatchScale;
+
+public sealed partial class View : UserControl
+{
+    public View()
+    {
+        InitializeComponent();
+    }
+}
