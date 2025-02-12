a24ff3fc7026d565962902562bdb8fcfb17f4468
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-02-11 21:35:28 -0600

    fixes #1780, Custom angular gauge needle
    Updates the web site to extend the angular gauge article, the article now inclides
    an example that changes the aspect of the angular gauge needle.

---
+++ samples/ViewModelsSamples/Pies/AngularGauge/CustomNeedle.cs
@@ -0,0 +1,14 @@
+﻿using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+
+namespace ViewModelsSamples.Pies.AngularGauge;
+
+public class CustomNeedle : NeedleGeometry
+{
+    public override void Draw(SkiaSharpDrawingContext context)
+    {
+        var paint = context.ActiveSkiaPaint;
+
+        context.Canvas.DrawRect(X - Width * 0.5f, Y, Width, Radius, paint);
+    }
+}
