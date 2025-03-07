3a9968bf123b78c2a12e3e32391a99bb45df46f6
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-03-07 12:59:07 -0600

    update livecharts configuration section

--- samples/WinFormsSample/Program.cs
+++ samples/WinFormsSample/Program.cs
@@ -1,40 +1,21 @@
 ﻿using System;
 using System.Windows.Forms;
-using LiveChartsCore; // mark
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.Themes; // mark
 
 namespace WinFormsSample;
 
 static class Program
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
+        // this is the recommended place to configure LiveCharts // mark
         LiveCharts.Configure(config => // mark
-           config // mark
-               // you can override the theme 
-               // .AddDarkTheme() // mark 
-
-               // In case you need a non-Latin based font, you must register a typeface for SkiaSharp
-               //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('汉')) // <- Chinese // mark
-               //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('あ')) // <- Japanese // mark
-               //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('헬')) // <- Korean // mark
-               //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('Ж'))  // <- Russian // mark
-
-               //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('أ'))  // <- Arabic // mark
-               //.UseRightToLeftSettings() // Enables right to left tooltips // mark
-
-               // finally register your own mappers
-               // you can learn more about mappers at:
-               // https://livecharts.dev/docs/{{ platform }}/{{ version }}/Overview.Mappers
-
-               // here we use the index as X, and the population as Y // mark
-               .HasMap<City>((city, index) => new(index, city.Population)) // mark
-               // .HasMap<Foo>( .... ) // mark
-               // .HasMap<Bar>( .... ) // mark
-       ); // mark
+        { // mark
+            // configure LiveCharts here... // mark
+        }); // mark;
 
         _ = Application.SetHighDpiMode(HighDpiMode.SystemAware);
         Application.EnableVisualStyles();
@@ -43,4 +24,10 @@ static class Program
     }
 
     public record City(string Name, double Population);
+}
+
+public class City
+{
+    public string Name { get; set; }
+    public double Population { get; set; }
 }
--- samples/WinUISample/WinUISample/App.xaml.cs
+++ samples/WinUISample/WinUISample/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
-using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView; // mark
+using LiveChartsCore; // mark
+using System;
 
 namespace WinUISample;
 
@@ -9,50 +9,21 @@ namespace WinUISample;
 /// </summary>
 public partial class App : Application
 {
-    public record City(string Name, double Population);
-
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
     public App()
     {
         InitializeComponent();
     }
 
-    /// <summary>
-    /// Invoked when the application is launched normally by the end user.  Other entry points
-    /// will be used such as when the application is launched to open a specific file.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         m_window = new MainWindow();
         m_window.Activate();
 
+        // this is the recommended place to configure LiveCharts // mark
         LiveCharts.Configure(config => // mark
-            config // mark
-                // you can override the theme 
-                //.AddDarkTheme() // mark 
-
-                // In case you need a non-Latin based font, you must register a typeface for SkiaSharp
-                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('汉')) // <- Chinese // mark
-                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('あ')) // <- Japanese // mark
-                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('헬')) // <- Korean // mark
-                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('Ж'))  // <- Russian // mark
-
-                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('أ'))  // <- Arabic // mark
-                //.UseRightToLeftSettings() // Enables right to left tooltips // mark
-
-                // finally register your own mappers
-                // you can learn more about mappers at:
-                // https://livecharts.dev/docs/{{ platform }}/{{ version }}/Overview.Mappers
-
-                // here we use the index as X, and the population as Y // mark
-                .HasMap<City>((city, index) => new(index, city.Population)) // mark
-                // .HasMap<Foo>( .... ) // mark
-                // .HasMap<Bar>( .... ) // mark
-            ); // mark
+        { // mark
+            // configure LiveCharts here... // mark
+        }); // mark
     }
 
     private Window m_window;
