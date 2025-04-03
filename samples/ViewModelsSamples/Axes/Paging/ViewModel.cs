b847a5fe6c245f68ffd24194fbb0e3212358b40c
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-04-02 21:22:28 -0600

    update CommunityToolkit.Mvvm

--- samples/ViewModelsSamples/Axes/Paging/ViewModel.cs
+++ samples/ViewModelsSamples/Axes/Paging/ViewModel.cs
@@ -1,64 +1,70 @@
 using System;
 using System.Collections.Generic;
-using CommunityToolkit.Mvvm.Input;
-using LiveChartsCore;
-using LiveChartsCore.Kernel.Sketches;
-using LiveChartsCore.SkiaSharpView;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ViewModelsSamples.Axes.Paging;
 
-public partial class ViewModel
+public partial class ViewModel : ObservableObject
 {
-    private readonly Random _random = new();
+    public int[] Values { get; set; } = Fetch();
 
-    public ISeries[] Series { get; }
+    [ObservableProperty]
+    public partial string Name { get; set; }
 
-    public ICartesianAxis[] XAxes { get; }
-
-    public ViewModel()
+    private static int[] Fetch()
     {
+        var random = new Random();
+
         var trend = 100;
         var values = new List<int>();
-
         for (var i = 0; i < 100; i++)
         {
-            trend += _random.Next(-30, 50);
+            trend += random.Next(-30, 50);
             values.Add(trend);
         }
 
-        Series = [new ColumnSeries<int>(values)];
-        XAxes = [new Axis()];
+        return [.. values];
     }
 
-    [RelayCommand]
-    public void GoToPage1()
-    {
-        var axis = XAxes[0];
-        axis.MinLimit = -0.5;
-        axis.MaxLimit = 10.5;
-    }
+    //public ISeries[] Series { get; }
 
-    [RelayCommand]
-    public void GoToPage2()
-    {
-        var axis = XAxes[0];
-        axis.MinLimit = 9.5;
-        axis.MaxLimit = 20.5;
-    }
+    //public ICartesianAxis[] XAxes { get; }
 
-    [RelayCommand]
-    public void GoToPage3()
-    {
-        var axis = XAxes[0];
-        axis.MinLimit = 19.5;
-        axis.MaxLimit = 30.5;
-    }
+    //public ViewModel()
+    //{
+    //    //Series = [new ColumnSeries<int>(values)];
+    //    XAxes = [new Axis()];
+    //}
 
-    [RelayCommand]
-    public void SeeAll()
-    {
-        var axis = XAxes[0];
-        axis.MinLimit = null;
-        axis.MaxLimit = null;
-    }
+    //[RelayCommand]
+    //public void GoToPage1()
+    //{
+    //    var axis = XAxes[0];
+    //    axis.MinLimit = -0.5;
+    //    axis.MaxLimit = 10.5;
+    //}
+
+    //[RelayCommand]
+    //public void GoToPage2()
+    //{
+    //    var axis = XAxes[0];
+    //    axis.MinLimit = 9.5;
+    //    axis.MaxLimit = 20.5;
+    //}
+
+    //[RelayCommand]
+    //public void GoToPage3()
+    //{
+    //    var axis = XAxes[0];
+    //    axis.MinLimit = 19.5;
+    //    axis.MaxLimit = 30.5;
+    //}
+
+    //[RelayCommand]
+    //public void SeeAll()
+    //{
+    //    var axis = XAxes[0];
+    //    axis.MinLimit = null;
+    //    axis.MaxLimit = null;
+    //}
 }
