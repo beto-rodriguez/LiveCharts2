d10faa45b91adc25ffef4217586d0c0e1017b974
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-04-04 11:44:26 -0600

    financial series in xaml

--- samples/ViewModelsSamples/Financial/BasicCandlesticks/ViewModel.cs
+++ samples/ViewModelsSamples/Financial/BasicCandlesticks/ViewModel.cs
@@ -1,55 +1,20 @@
 ﻿using System;
-using System.Linq;
-using LiveChartsCore;
+using System.Collections.ObjectModel;
 using LiveChartsCore.Defaults;
-using LiveChartsCore.SkiaSharpView;
 
 namespace ViewModelsSamples.Financial.BasicCandlesticks;
 
 public class ViewModel
 {
-    public Axis[] XAxes { get; set; }
+    public ObservableCollection<FinancialPoint> Values { get; set; } = [
+        new() { Date = new DateTime(2021, 1, 1), High = 523, Open = 500, Close = 450, Low = 400 },
+        new() { Date = new DateTime(2021, 1, 2), High = 500, Open = 450, Close = 425, Low = 400 },
+        new() { Date = new DateTime(2021, 1, 3), High = 490, Open = 425, Close = 400, Low = 380 },
+        new() { Date = new DateTime(2021, 1, 4), High = 420, Open = 400, Close = 420, Low = 380 },
+        new() { Date = new DateTime(2021, 1, 5), High = 520, Open = 420, Close = 490, Low = 400 },
+        new() { Date = new DateTime(2021, 1, 6), High = 580, Open = 490, Close = 560, Low = 440 }
+    ];
 
-    public ISeries[] Series { get; set; }
-
-    public ViewModel()
-    {
-        var data = new FinancialData[]
-        {
-            new() { Date = new DateTime(2021, 1, 1), High = 523, Open = 500, Close = 450, Low = 400 },
-            new() { Date = new DateTime(2021, 1, 2), High = 500, Open = 450, Close = 425, Low = 400 },
-            new() { Date = new DateTime(2021, 1, 3), High = 490, Open = 425, Close = 400, Low = 380 },
-            new() { Date = new DateTime(2021, 1, 4), High = 420, Open = 400, Close = 420, Low = 380 },
-            new() { Date = new DateTime(2021, 1, 5), High = 520, Open = 420, Close = 490, Low = 400 },
-            new() { Date = new DateTime(2021, 1, 6), High = 580, Open = 490, Close = 560, Low = 440 }
-        };
-
-        Series = [
-            new CandlesticksSeries<FinancialPointI>
-            {
-                Values = data
-                    .Select(x => new FinancialPointI(x.High, x.Open, x.Close, x.Low))
-                    .ToArray()
-            }
-        ];
-
-        XAxes = [
-            new Axis
-            {
-                LabelsRotation = 15,
-                Labels = data
-                    .Select(x => x.Date.ToString("yyyy MMM dd"))
-                    .ToArray()
-            }
-        ];
-    }
+    public Func<DateTime, string> DateFormatter { get; set; } = value => value.ToString("yyyy MMM dd");
 }
 
-public class FinancialData
-{
-    public DateTime Date { get; set; }
-    public double High { get; set; }
-    public double Open { get; set; }
-    public double Close { get; set; }
-    public double Low { get; set; }
-}
