09a585161151a63399f31630cb08dfaabd2c445e
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-06-20 10:49:38 -0600

    update bars/custom sample

--- samples/ViewModelsSamples/Bars/Custom/ViewModel.cs
+++ samples/ViewModelsSamples/Bars/Custom/ViewModel.cs
@@ -2,5 +2,8 @@
 
 public class ViewModel
 {
-    // nothing here :(
+    public double[] Values1 { get; } = [2, 1, 4];
+    public double[] Values2 { get; } = [4, 3, 6];
+    public double[] Values3 { get; } = [-2, 2, 1];
+    public double[] Values4 { get; } = [1, 2, 3];
 }
