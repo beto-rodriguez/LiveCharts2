2a3fa1d8f713cd5c826bd885b3b6884680896580
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-06-29 09:53:55 -0600

    winforms missing samples updates

--- samples/WinFormsSample/Polar/RadialArea/View.cs
+++ samples/WinFormsSample/Polar/RadialArea/View.cs
@@ -1,28 +1,55 @@
 ﻿using System.Windows.Forms;
 using LiveChartsCore.SkiaSharpView.WinForms;
-using ViewModelsSamples.Polar.RadialArea;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace WinFormsSample.Polar.RadialArea;
 
 public partial class View : UserControl
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="View"/> class.
-    /// </summary>
     public View()
     {
         InitializeComponent();
         Size = new System.Drawing.Size(50, 50);
 
-        var viewModel = new ViewModel();
+        var values1 = new int[] { 7, 5, 7, 5, 6 };
+        var values2 = new int[] { 2, 7, 5, 9, 7 };
+        var labels = new string[] { "first", "second", "third", "forth", "fifth" };
+        var tangentAngle = LiveCharts.TangentAngle;
+
+        var series = new ISeries[]
+        {
+            new PolarLineSeries<int>
+            {
+                Values = values1,
+                LineSmoothness = 0,
+                GeometrySize = 0,
+                Fill = new SolidColorPaint(SKColor.Parse("#900000ff"))
+            },
+            new PolarLineSeries<int>
+            {
+                Values = values2,
+                LineSmoothness = 1,
+                GeometrySize = 0,
+                Fill = new SolidColorPaint(SKColor.Parse("#90ff0000"))
+            }
+        };
+
+        var angleAxes = new PolarAxis[]
+        {
+            new() {
+                LabelsRotation = tangentAngle,
+                Labels = labels
+            }
+        };
 
         var polarChart = new PolarChart
         {
-            Series = viewModel.Series,
-            AngleAxes = viewModel.AngleAxes,
+            Series = series,
+            AngleAxes = angleAxes,
             InitialRotation = -45,
-
-            // out of livecharts properties...
             Location = new System.Drawing.Point(0, 0),
             Size = new System.Drawing.Size(50, 50),
             Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom
--- samples/WinFormsSample/StepLines/Basic/View.cs
+++ samples/WinFormsSample/StepLines/Basic/View.cs
@@ -1,25 +1,46 @@
 ﻿using System.Windows.Forms;
 using LiveChartsCore.SkiaSharpView.WinForms;
-using ViewModelsSamples.StepLines.Basic;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
 
 namespace WinFormsSample.StepLines.Basic;
 
 public partial class View : UserControl
 {
-    private readonly CartesianChart cartesianChart;
-
     public View()
     {
         InitializeComponent();
         Size = new System.Drawing.Size(50, 50);
 
-        var viewModel = new ViewModel();
+        var values = new double[] { 2, 1, 3, 4, 3, 4, 6 };
 
-        cartesianChart = new CartesianChart
+        var series = new ISeries[]
         {
-            Series = viewModel.Series,
+            new StepLineSeries<double>
+            {
+                Values = values,
+                Fill = null,
+                GeometrySize = 20
+            }
+        };
 
-            // out of livecharts properties...
+        var title = new DrawnLabelVisual(
+            new LabelGeometry
+            {
+                Text = "My chart title",
+                Paint = new SolidColorPaint(SKColor.Parse("#303030")),
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15)
+            });
+
+        var cartesianChart = new CartesianChart
+        {
+            Series = series,
+            Title = title,
             Location = new System.Drawing.Point(0, 0),
             Size = new System.Drawing.Size(50, 50),
             Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom
