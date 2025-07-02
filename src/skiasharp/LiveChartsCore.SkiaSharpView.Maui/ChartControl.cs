338702628f8075ec6563a9b27e12bc2d4c4f540e
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-07-02 13:40:50 -0600

    use SG in maui contrlos

---
+++ src/skiasharp/LiveChartsCore.SkiaSharpView.Maui/ChartControl.cs
@@ -0,0 +1,269 @@
+// The MIT License(MIT)
+//
+// Copyright(c) 2021 Alberto Rodriguez Orozco & LiveCharts Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LiveChartsCore.Drawing;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Kernel.Events;
+using LiveChartsCore.Kernel.Observers;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.Measure;
+using LiveChartsCore.Motion;
+using LiveChartsCore.Themes;
+using LiveChartsCore.VisualElements;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace LiveChartsCore.SkiaSharpView.Maui;
+
+/// <inheritdoc cref="IChartView"/>
+public abstract partial class ChartControl : ChartView, IChartView
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChartControl"/> class.
+    /// </summary>
+    protected ChartControl()
+    {
+        LiveCharts.Configure(config => config.UseDefaults());
+
+        Content = new MotionCanvas();
+        CoreChart = CreateCoreChart();
+
+        CoreChart.Measuring += OnCoreMeasuring;
+        CoreChart.UpdateStarted += OnCoreUpdateStarted;
+        CoreChart.UpdateFinished += OnCoreUpdateFinished;
+
+        SizeChanged += (s, e) =>
+            CoreChart.Update();
+
+        Observe = new ChartObserver(() => CoreChart?.Update(), AddUIElement, RemoveUIElement);
+
+        if (Application.Current is not null)
+            Application.Current.RequestedThemeChanged += (sender, args) => CoreChart?.ApplyTheme();
+    }
+
+    /// <summary>
+    /// Gets the canvas view.
+    /// </summary>
+    public MotionCanvas CanvasView => (MotionCanvas)Content;
+
+    /// <summary>
+    /// Gets the core chart.
+    /// </summary>
+    public Chart CoreChart { get; }
+
+    /// <summary>
+    /// Gets the chart observer.
+    /// </summary>
+    protected ChartObserver Observe { get; }
+
+    bool IChartView.DesignerMode => false;
+    bool IChartView.IsDarkMode => Application.Current?.RequestedTheme == AppTheme.Dark;
+    LvcColor IChartView.BackColor
+    {
+        get => Background is not SolidColorBrush b
+            ? new LvcColor()
+            : LvcColor.FromArgb(
+                (byte)(b.Color.Alpha * 255), (byte)(b.Color.Red * 255),
+                (byte)(b.Color.Green * 255), (byte)(b.Color.Blue * 255));
+        set => Background = new SolidColorBrush(Color.FromRgba(value.R / 255, value.G / 255, value.B / 255, value.A / 255));
+    }
+    LvcSize IChartView.ControlSize => new() { Width = (float)CanvasView.Width, Height = (float)CanvasView.Height };
+
+    /// <inheritdoc cref="IChartView.Tooltip" />
+    public IChartTooltip? Tooltip { get; set; }
+
+    /// <inheritdoc cref="IChartView.Legend" />
+    public IChartLegend? Legend { get => field; set { field = value; CoreChart.Update(); } }
+
+    /// <inheritdoc cref="IChartView.ChartTheme" />
+    public Theme? ChartTheme { get; set { field = value; OnChartPropertyChanged(this); } }
+
+    /// <inheritdoc cref="IChartView.CoreCanvas" />
+    public CoreMotionCanvas CoreCanvas => CanvasView.CanvasCore;
+
+    /// <inheritdoc cref="IChartView.Measuring" />
+    public event ChartEventHandler? Measuring;
+
+    /// <inheritdoc cref="IChartView.UpdateFinished" />
+    public event ChartEventHandler? UpdateFinished;
+
+    /// <inheritdoc cref="IChartView.UpdateStarted" />
+    public event ChartEventHandler? UpdateStarted;
+
+    /// <inheritdoc cref="IChartView.DataPointerDown" />
+    public event ChartPointsHandler? DataPointerDown;
+
+    /// <inheritdoc cref="IChartView.HoveredPointsChanged" />
+    public event ChartPointHoverHandler? HoveredPointsChanged;
+
+    /// <inheritdoc cref="IChartView.ChartPointPointerDown" />
+    [Obsolete($"Use the {nameof(DataPointerDown)} event instead with a {nameof(FindingStrategy)} that used TakeClosest.")]
+    public event ChartPointHandler? ChartPointPointerDown;
+
+    /// <inheritdoc cref="IChartView.VisualElementsPointerDown"/>
+    public event VisualElementsHandler? VisualElementsPointerDown;
+
+    /// <inheritdoc cref="NavigableElement.OnParentSet"/>
+    protected override void OnParentSet()
+    {
+        base.OnParentSet();
+
+        if (Parent == null)
+        {
+            Observe.Dispose();
+            CoreChart?.Unload();
+            return;
+        }
+
+        CoreChart?.Load();
+    }
+
+    /// <summary>
+    /// Creates the core chart instance for rendering and manipulation.
+    /// </summary>
+    /// <remarks>This method is abstract and must be implemented by derived classes to provide     a specific
+    /// chart type. The returned <see cref="Chart"/> object represents the     foundational chart structure, which can
+    /// be further customized or populated     with data.</remarks>
+    /// <returns>A <see cref="Chart"/> object that serves as the base chart instance.</returns>
+    protected abstract Chart CreateCoreChart();
+
+    private void OnCoreUpdateFinished(IChartView chart) => UpdateFinished?.Invoke(this);
+
+    private void OnCoreUpdateStarted(IChartView chart)
+    {
+        if (UpdateStartedCommand is not null)
+        {
+            var args = new ChartCommandArgs(this);
+            if (UpdateStartedCommand.CanExecute(args))
+                UpdateStartedCommand.Execute(args);
+        }
+
+        UpdateStarted?.Invoke(this);
+    }
+
+    private void OnCoreMeasuring(IChartView chart) => Measuring?.Invoke(this);
+
+    private void AddUIElement(object item)
+    {
+        if (item is not View view) return;
+        CanvasView.Children.Add(view);
+    }
+
+    private void RemoveUIElement(object item)
+    {
+        if (item is not View view) return;
+        _ = CanvasView.Children.Remove(view);
+    }
+
+    internal override void OnPressed(object? sender, Behaviours.Events.PressedEventArgs args)
+    {
+        // not implemented yet?
+        // https://github.com/dotnet/maui/issues/16202
+        //if (Keyboard.Modifiers > 0) return;
+
+        var cArgs = new PointerCommandArgs(this, new(args.Location.X, args.Location.Y), args);
+        if (PressedCommand?.CanExecute(cArgs) == true)
+            PressedCommand.Execute(cArgs);
+
+        CoreChart.InvokePointerDown(args.Location, args.IsSecondaryPress);
+    }
+
+    internal override void OnMoved(object? sender, Behaviours.Events.ScreenEventArgs args)
+    {
+        var location = args.Location;
+
+        var cArgs = new PointerCommandArgs(this, new(location.X, location.Y), args.OriginalEvent);
+        if (MovedCommand?.CanExecute(cArgs) == true)
+            MovedCommand.Execute(cArgs);
+
+        CoreChart.InvokePointerMove(location);
+    }
+
+    internal override void OnReleased(object? sender, Behaviours.Events.PressedEventArgs args)
+    {
+        var cArgs = new PointerCommandArgs(this, new(args.Location.X, args.Location.Y), args);
+        if (ReleasedCommand?.CanExecute(cArgs) == true)
+            ReleasedCommand.Execute(cArgs);
+
+        CoreChart.InvokePointerUp(args.Location, args.IsSecondaryPress);
+    }
+
+    internal override void OnExited(object? sender, Behaviours.Events.EventArgs args) =>
+        CoreChart.InvokePointerLeft();
+
+    /// <inheritdoc cref="IChartView.GetPointsAt(LvcPointD, FindingStrategy, FindPointFor)"/>
+    public IEnumerable<ChartPoint> GetPointsAt(
+        LvcPointD point,
+        FindingStrategy strategy = FindingStrategy.Automatic,
+        FindPointFor findPointFor = FindPointFor.HoverEvent)
+    {
+        if (strategy == FindingStrategy.Automatic)
+            strategy = CoreChart.Series.GetFindingStrategy();
+
+        return CoreChart.Series.SelectMany(series =>
+            series.FindHitPoints(CoreChart, new(point), strategy, FindPointFor.HoverEvent));
+    }
+
+    /// <inheritdoc cref="IChartView.GetVisualsAt(LvcPointD)"/>
+    public IEnumerable<IChartElement> GetVisualsAt(LvcPointD point) =>
+        CoreChart.VisualElements.SelectMany(visual =>
+            ((LiveChartsCore.VisualElements.VisualElement)visual).IsHitBy(CoreChart, new(point)));
+
+    void IChartView.InvokeOnUIThread(Action action) => _ = MainThread.InvokeOnMainThreadAsync(action);
+
+    void IChartView.Invalidate() => CoreCanvas.Invalidate();
+
+    void IChartView.OnDataPointerDown(IEnumerable<ChartPoint> points, LvcPoint pointer)
+    {
+        DataPointerDown?.Invoke(this, points);
+        if (DataPointerDownCommand is not null && DataPointerDownCommand.CanExecute(points))
+            DataPointerDownCommand.Execute(points);
+
+        ChartPointPointerDown?.Invoke(this, points.FindClosestTo(pointer));
+#pragma warning disable CS0618 // Type or member is obsolete
+        ChartPointPointerDownCommand?.Execute(points.FindClosestTo(pointer));
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    void IChartView.OnHoveredPointsChanged(IEnumerable<ChartPoint>? newPoints, IEnumerable<ChartPoint>? oldPoints)
+    {
+        HoveredPointsChanged?.Invoke(this, newPoints, oldPoints);
+
+        var args = new HoverCommandArgs(this, newPoints, oldPoints);
+        if (HoveredPointsChangedCommand is not null && HoveredPointsChangedCommand.CanExecute(args))
+            HoveredPointsChangedCommand.Execute(args);
+    }
+
+    void IChartView.OnVisualElementPointerDown(
+        IEnumerable<IInteractable> visualElements, LvcPoint pointer)
+    {
+        var args = new VisualElementsEventArgs(CoreChart, visualElements, pointer);
+
+        VisualElementsPointerDown?.Invoke(this, args);
+        if (VisualElementsPointerDownCommand is not null && VisualElementsPointerDownCommand.CanExecute(args))
+            VisualElementsPointerDownCommand.Execute(args);
+    }
+}
--- src/skiasharp/LiveChartsCore.SkiaSharpView.Maui/ChartView.cs
+++ src/skiasharp/LiveChartsCore.SkiaSharpView.Maui/ChartView.cs
@@ -21,9 +21,6 @@
 // SOFTWARE.
 
 using System;
-using System.Collections.Generic;
-using LiveChartsCore.Generators;
-using LiveChartsCore.Kernel.Observers;
 using Microsoft.Maui.Controls;
 
 namespace LiveChartsCore.SkiaSharpView.Maui;
@@ -44,105 +41,10 @@ public abstract partial class ChartView : ContentView
         }
     }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ChartView"/> class.
-    /// </summary>
-    protected ChartView()
-    {
-        Content = new MotionCanvas();
-        Observe = new ChartObserver(() => CoreChart?.Update(), AddUIElement, RemoveUIElement)
-        {
-            {
-                nameof(SeriesSource),
-                new SeriesSourceObserver(
-                    InflateSeriesTemplate,
-                    GetSeriesSource,
-                    () => SeriesSource is not null && SeriesTemplate is not null)
-            }
-        };
-    }
-
-    /// <summary>
-    /// Gets the canvas view.
-    /// </summary>
-    public MotionCanvas CanvasView => (MotionCanvas)Content;
-
-    /// <summary>
-    /// Gets the core chart.
-    /// </summary>
-    public abstract Chart CoreChart { get; }
-
-    /// <summary>
-    /// Gets the chart observer.
-    /// </summary>
-    protected ChartObserver Observe { get; }
-
-    /// <summary>
-    /// Gets or sets the series.
-    /// </summary>
-    public abstract ICollection<ISeries> Series { get; set; }
-
-    #region Generated Bindable Properties
-
-#pragma warning disable IDE1006 // Naming Styles
-#pragma warning disable IDE0052 // Remove unread private member
-
-    private static readonly XamlProperty<IEnumerable<object>> seriesSource = new(onChanged: OnSeriesSourceChanged);
-    private static readonly XamlProperty<DataTemplate> seriesTemplate = new(onChanged: OnSeriesSourceChanged);
-
-#pragma warning restore IDE1006 // Naming Styles
-#pragma warning restore IDE0052 // Remove unread private members
-
-    #endregion
-
     internal virtual void OnPressed(object? sender, Behaviours.Events.PressedEventArgs args) { }
     internal virtual void OnMoved(object? sender, Behaviours.Events.ScreenEventArgs args) { }
     internal virtual void OnReleased(object? sender, Behaviours.Events.PressedEventArgs args) { }
     internal virtual void OnScrolled(object? sender, Behaviours.Events.ScrollEventArgs args) { }
     internal virtual void OnPinched(object? sender, Behaviours.Events.PinchEventArgs args) { }
     internal virtual void OnExited(object? sender, Behaviours.Events.EventArgs args) { }
-
-    private void AddUIElement(object item)
-    {
-        if (item is not View view) return;
-        CanvasView.Children.Add(view);
-    }
-
-    private void RemoveUIElement(object item)
-    {
-        if (item is not View view) return;
-        _ = CanvasView.Children.Remove(view);
-    }
-
-    private static void OnSeriesSourceChanged(BindableObject bindable, object oldValue, object newValue)
-    {
-        var chart = (ChartView)bindable;
-
-        var seriesObserver = (SeriesSourceObserver)chart.Observe[nameof(SeriesSource)];
-        seriesObserver.Initialize(chart.SeriesSource);
-
-        if (seriesObserver.Series is not null)
-            chart.Series = seriesObserver.Series;
-    }
-
-    private ISeries InflateSeriesTemplate(object item)
-    {
-        if (SeriesTemplate.CreateContent() is not View template)
-            throw new InvalidOperationException("The template must be a View.");
-        if (template is not ISeries series)
-            throw new InvalidOperationException("The template is not a valid series.");
-
-        template.BindingContext = item;
-
-        return series;
-    }
-
-    private static object GetSeriesSource(ISeries series) => ((View)series).BindingContext;
-
-    /// <summary>
-    /// Initializes the observer for a property change.
-    /// </summary>
-    /// <param name="propertyName">The property.</param>
-    protected static BindableProperty.BindingPropertyChangedDelegate InitializeObserver(string propertyName) =>
-        (bo, o, n) => ((ChartView)bo).Observe[propertyName].Initialize(n);
 }
