40e6b2552ae159a127661bb4cd7f2777d89f3b27
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-07-08 14:34:16 -0600

    add WinUI.Shared

---
+++ src/skiasharp/_Shared.WinUI/ChartBehaviour.cs
@@ -0,0 +1,170 @@
+﻿// The MIT License(MIT)
+//
+// Copyright(c) 2021 Alberto Rodriguez Orozco & LiveCharts Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using LiveChartsCore.Drawing;
+using Microsoft.UI.Xaml;
+
+namespace LiveChartsCore.SkiaSharpView.WinUI;
+
+/// <summary>
+/// The chart behaviour for WinUI and Uno Platform.
+/// </summary>
+public partial class ChartBehaviour : Behaviours.ChartBehaviour
+{
+    private static double s_density;
+    private static LvcSize s_screenSize;
+
+    /// <inheritdoc />
+    public override LvcSize ScreenSize => s_screenSize;
+
+    /// <inheritdoc />
+    public override double Density => s_density;
+
+    /// <summary>
+    /// Attaches the native events on the specified element.
+    /// </summary>
+    /// <param name="element">The element.</param>
+    public void On(FrameworkElement element)
+    {
+        // TODO: Detect the DPI and screen size changes.
+
+#if HAS_UNO_WINUI
+        var currentView = Windows.Graphics.Display.DisplayInformation.GetForCurrentView();
+
+        s_density = currentView.LogicalDpi / 96.0f;
+        s_screenSize = new(
+            currentView.ScreenWidthInRawPixels,
+            currentView.ScreenHeightInRawPixels);
+#else
+        void getDensity(object s, RoutedEventArgs e)
+        {
+            s_density = element.XamlRoot.RasterizationScale;
+            element.Loaded -= getDensity;
+        }
+
+        if (element.IsLoaded) getDensity(null!, null!);
+        else element.Loaded += getDensity;
+#endif
+
+#if ANDROID
+
+        element.Touch += OnAndroidTouched;
+        element.Hover += OnAndroidHover;
+
+#elif MACCATALYST || IOS
+
+        element.UserInteractionEnabled = true;
+#if MACCATALYST
+        element.AddGestureRecognizer(MacCatalystHoverGestureRecognizer);
+#endif
+        element.AddGestureRecognizer(MacCatalystLongPressGestureRecognizer);
+        element.AddGestureRecognizer(MacCatalystPinchGestureRecognizer);
+        element.AddGestureRecognizer(MacCatalystPanGestureRecognizer);
+
+#elif WINDOWS
+
+        element.PointerPressed += OnWindowsPointerPressed;
+        element.PointerMoved += OnWindowsPointerMoved;
+        element.PointerReleased += OnWindowsPointerReleased;
+        element.PointerWheelChanged += OnWindowsPointerWheelChanged;
+        element.PointerExited += OnWindowsPointerExited;
+
+#elif HAS_UNO || HAS_UNO_WINUI
+
+        element.PointerPressed += OnUnoPointerPressed;
+        element.PointerMoved += OnUnoPointerMoved;
+        element.PointerReleased += OnUnoPointerReleased;
+        element.PointerWheelChanged += OnUnoPointerWheelChanged;
+        element.PointerExited += OnUnoPointerExited;
+
+#endif
+    }
+
+#if (HAS_UNO || HAS_UNO_WINUI) && !ANDROID && !IOS && !MACCATALYST && !WINDOWS
+    // is this just wasm?
+
+    /// <summary>
+    /// On uno pointer pressed.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void OnUnoPointerPressed(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        var p = e.GetCurrentPoint(sender as UIElement);
+        if (p is null) return;
+
+        InvokePressed(
+            sender,
+            new(new(p.Position.X, p.Position.Y), p.Properties.IsRightButtonPressed, e));
+    }
+
+    /// <summary>
+    /// On uno pointer moved.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void OnUnoPointerMoved(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        var p = e.GetCurrentPoint(sender as UIElement);
+        if (p is null) return;
+
+        InvokeMoved(
+            sender,
+            new(new(p.Position.X, p.Position.Y), e));
+    }
+
+    /// <summary>
+    /// On uno pointer released.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void OnUnoPointerReleased(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        var p = e.GetCurrentPoint(sender as UIElement);
+        if (p is null) return;
+
+        InvokeReleased(
+            sender,
+            new(new(p.Position.X, p.Position.Y), p.Properties.IsRightButtonPressed, e));
+    }
+
+    /// <summary>
+    /// On uno pointer wheel changed.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void OnUnoPointerWheelChanged(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        var p = e.GetCurrentPoint(sender as UIElement);
+
+        InvokeScrolled(sender, new(new(p.Position.X, p.Position.Y), p.Properties.MouseWheelDelta, e));
+    }
+
+    /// <summary>
+    /// On uno pointer exited.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void OnUnoPointerExited(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e) =>
+        InvokeExited(sender, new(e));
+#endif
+}
---
+++ src/skiasharp/_Shared.WinUI/ChartControl.cs
@@ -0,0 +1,197 @@
+﻿// The MIT License(MIT)
+//
+// Copyright(c) 2021 Alberto Rodriguez Orozco & LiveCharts Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+using LiveChartsCore.Drawing;
+using LiveChartsCore.Kernel.Events;
+using LiveChartsCore.Kernel.Sketches;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace LiveChartsCore.SkiaSharpView.WinUI;
+
+/// <inheritdoc cref="IChartView"/>
+public abstract partial class ChartControl : UserControl, IChartView
+{
+    private readonly ThemeListener _themeListener;
+    private static readonly bool s_isWebAssembly = RuntimeInformation.IsOSPlatform(OSPlatform.Create("BROWSER"));
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChartControl"/> class.
+    /// </summary>
+    public ChartControl()
+    {
+        LiveCharts.Configure(config => config.UseDefaults());
+
+        Content = new MotionCanvas();
+
+        InitializeCoreChart();
+        InitializeObservers();
+
+        SizeChanged += (s, e) =>
+            CoreChart.Update();
+
+        Loaded += OnLoaded;
+        Unloaded += OnUnloaded;
+
+        SizeChanged += (s, e) =>
+            CoreChart.Update();
+
+        _themeListener = new(CoreChart.ApplyTheme, DispatcherQueue);
+
+        // We use the behaviours assembly to share support for Uno
+        var chartBehaviour = new ChartBehaviour();
+
+        chartBehaviour.Pressed += OnPressed;
+        chartBehaviour.Moved += OnMoved;
+        chartBehaviour.Released += OnReleased;
+        chartBehaviour.Scrolled += OnScrolled;
+        chartBehaviour.Pinched += OnPinched;
+        chartBehaviour.Exited += OnExited;
+
+        chartBehaviour.On(this);
+    }
+
+    /// <summary>
+    /// Gets the canvas view.
+    /// </summary>
+    public MotionCanvas CanvasView => (MotionCanvas)Content;
+
+    bool IChartView.DesignerMode => false;
+    bool IChartView.IsDarkMode => Application.Current?.RequestedTheme == ApplicationTheme.Dark;
+    LvcColor IChartView.BackColor
+    {
+        get => Background is not SolidColorBrush b
+            ? new LvcColor()
+            : LvcColor.FromArgb(b.Color.A, b.Color.R, b.Color.G, b.Color.B);
+        set => SetValue(BackgroundProperty, new SolidColorBrush(Windows.UI.Color.FromArgb(value.A, value.R, value.G, value.B)));
+    }
+    LvcSize IChartView.ControlSize => new() { Width = (float)CanvasView.ActualWidth, Height = (float)CanvasView.ActualHeight };
+
+    private void OnLoaded(object sender, RoutedEventArgs e) =>
+        CoreChart.Load();
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        _themeListener.Dispose();
+        Observe.Dispose();
+        CoreChart.Unload();
+    }
+
+    private void AddUIElement(object item)
+    {
+        if (CanvasView is null || item is not UIElement uiElement) return;
+        CanvasView.Children.Add(uiElement);
+    }
+
+    private void RemoveUIElement(object item)
+    {
+        if (CanvasView is null || item is not UIElement uiElement) return;
+        _ = CanvasView.Children.Remove(uiElement);
+    }
+
+    private void OnPressed(object? sender, Behaviours.Events.PressedEventArgs args)
+    {
+        // is this working on all platforms?
+        //if (args.KeyModifiers > 0) return;
+
+        var cArgs = new PointerCommandArgs(this, new(args.Location.X, args.Location.Y), args);
+        if (PointerPressedCommand?.CanExecute(cArgs) == true)
+            PointerPressedCommand.Execute(cArgs);
+
+        CoreChart?.InvokePointerDown(args.Location, args.IsSecondaryPress);
+    }
+
+    private void OnMoved(object? sender, Behaviours.Events.ScreenEventArgs args)
+    {
+        var location = args.Location;
+
+        var cArgs = new PointerCommandArgs(this, new(location.X, location.Y), args.OriginalEvent);
+        if (PointerMoveCommand?.CanExecute(cArgs) == true)
+            PointerMoveCommand.Execute(cArgs);
+
+        CoreChart?.InvokePointerMove(location);
+    }
+
+    private void OnReleased(object? sender, Behaviours.Events.PressedEventArgs args)
+    {
+        var cArgs = new PointerCommandArgs(this, new(args.Location.X, args.Location.Y), args);
+        if (PointerReleasedCommand?.CanExecute(cArgs) == true)
+            PointerReleasedCommand.Execute(cArgs);
+
+        CoreChart?.InvokePointerUp(args.Location, args.IsSecondaryPress);
+    }
+
+    private void OnExited(object? sender, Behaviours.Events.EventArgs args) =>
+        CoreChart?.InvokePointerLeft();
+
+    /// <summary>
+    /// Invoked when a scroll event occurs, allowing derived classes to handle or respond to the event.
+    /// </summary>
+    /// <remarks>This method is designed to be overridden in derived classes to provide custom handling for
+    /// scroll events. If not overridden, the base implementation does nothing.</remarks>
+    /// <param name="sender">The source of the scroll event. This can be <see langword="null"/> if the event is not associated with a
+    /// specific sender.</param>
+    /// <param name="args">The event data containing details about the scroll action, such as scroll direction and position.</param>
+    protected virtual void OnScrolled(object? sender, Behaviours.Events.ScrollEventArgs args) { }
+
+    /// <summary>
+    /// Handles the pinch gesture event, allowing zooming functionality in the chart.
+    /// </summary>
+    /// <remarks>This method is invoked when a pinch gesture is detected. It enables zooming in or out of the chart
+    /// based on the scroll delta provided in the event arguments. Override this method in a derived class to customize the
+    /// behavior of pinch gestures.</remarks>
+    /// <param name="sender">The source of the event. This can be <see langword="null"/>.</param>
+    /// <param name="args">The event data containing information about the pinch gesture, including its location and scroll delta.</param>
+    protected virtual void OnPinched(object? sender, Behaviours.Events.PinchEventArgs args) { }
+
+    private ISeries InflateSeriesTemplate(object item)
+    {
+        var content = (FrameworkElement)SeriesTemplate.LoadContent();
+
+        if (content is not ISeries series)
+            throw new InvalidOperationException("The template must be a valid series.");
+
+        content.DataContext = item;
+
+        return series;
+    }
+
+    private static object GetSeriesSource(ISeries series) =>
+        ((FrameworkElement)series).DataContext!;
+
+    void IChartView.InvokeOnUIThread(Action action)
+    {
+        if (s_isWebAssembly)
+        {
+            // IF UNO WASM, just run the action directly.
+            // is this required on wasm isnt this already implemented in net 9?
+            action();
+            return;
+        }
+
+        _ = DispatcherQueue.TryEnqueue(
+            Microsoft.UI.Dispatching.DispatcherQueuePriority.Normal, () => action());
+    }
+}
