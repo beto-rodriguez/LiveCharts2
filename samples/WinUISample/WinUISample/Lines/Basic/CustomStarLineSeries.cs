9b07025813da777f1ba552f2f23cd0bce5b43bd4
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-07-11 21:09:44 -0600

    move winui samples to samples folder

--- samples/WinUISample/WinUISample/Lines/Basic/CustomStarLineSeries.cs
+++
@@ -1,9 +0,0 @@
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
-using LiveChartsCore.SkiaSharpView.WinUI;
-
-namespace WinUISample.Lines.Basic;
-
-// WinUI xaml parser does not support generic types
-// we instead create a non-generic class that inherits from the generic one.
-public class CustomStarLineSeries : XamlLineSeries<int, StarGeometry> { }
--- samples/WinUISample/WinUISample/Lines/CustomPoints/CustomArrowLineSeries.cs
+++
@@ -1,9 +0,0 @@
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.WinUI;
-using ViewModelsSamples.Lines.CustomPoints;
-
-namespace WinUISample.Lines.CustomPoints;
-
-// WinUI xaml parser does not support generic types
-// we instead create a non-generic class that inherits from the generic one.
-public class CustomArrowLineSeries : XamlLineSeries<DataPoint, ArrowGeometry> { }
