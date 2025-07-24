a13aef59e3279cfe95f6f740a3fba70403ecd974
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-07-24 13:52:56 -0600

    Add Uno desktop target, remove fallback

--- src/_Shared.Native/NativeTicker.Windows.cs
+++ src/_Shared.Native/NativeTicker.Windows.cs
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#if WINDOWS
+#if WINDOWS || DESKTOP
 
 using LiveChartsCore.Motion;
 using Microsoft.UI.Xaml.Media;
